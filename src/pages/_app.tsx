9dbcad68ae2dcb88df278cce562e9fe8b46d4acf
0xbono <iam@sonjim.in>
2021-01-27 22:04:35 +0900

    chore(app): 다크모드 및 라이트 모드 대응을 위한 ThemeProvider 추가

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,17 @@
 import React, { useEffect } from 'react';
+import { ThemeProvider } from '@emotion/react';
 import { NextComponentType } from 'next';
 import Head from 'next/head';
 import { ApolloProvider } from '@apollo/client';
 import { AppContext, AppInitialProps, AppProps } from 'next/app';
 import { LocalStorageWrapper, persistCache } from 'apollo3-cache-persist';
 import { cache, useApollo } from '../lib/apolloClient';
+import { useTheme } from '../hooks';
 
 const App: NextComponentType<AppContext, AppInitialProps, AppProps> = ({ Component, pageProps }) => {
+  const { isTheme, changeMode } = useTheme();
+  const theme = isTheme === 'light' ? '' : '';
+
   const apolloClient = useApollo(pageProps);
   const initialAction = async () => {
     await persistCache({
@@ -24,7 +29,9 @@ const App: NextComponentType<AppContext, AppInitialProps, AppProps> = ({ Compone
         <meta name="viewport" content="width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=1,user-scalable=no" />
       </Head>
       <ApolloProvider client={apolloClient}>
-        <Component {...pageProps} />
+        <ThemeProvider theme={theme}>
+          <Component {...pageProps} />
+        </ThemeProvider>
       </ApolloProvider>
     </>
   );
