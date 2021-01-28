d1835e79b8332db490743ba04a26a6dc9d3301b2
0xbono <iam@sonjim.in>
2021-01-28 21:49:25 +0900

    feat(sb): 스토리북 테마 변경 및 캔버스 히든 처리

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,8 +1,24 @@
+import { addParameters } from '@storybook/react';
 import { themes } from '@storybook/theming';
 
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
   docs: {
-    theme: themes.dark,
+    theme: themes.light,
+    page: null,
   },
 }
+
+addParameters({
+  previewTabs: {
+    canvas: {
+      hidden: true,
+    }
+  },
+  options: {
+    showPanel: false,
+    isFullscreen: false,
+    storySort: undefined,
+    isToolshown: true,
+  },
+})
