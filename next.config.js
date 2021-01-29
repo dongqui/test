b0b5c55a4c198faa509759d9d00990107faf9624
0xbono <iam@sonjim.in>
2021-01-29 12:52:45 +0900

    fix(vercel): next.js에서 플러그인 여러개 사용할 수 있도록 변경

--- next.config.js
+++ next.config.js
@@ -1,38 +1,75 @@
 const withPWA = require('next-pwa');
+const withPlugins = require('next-compose-plugins');
 const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin');
 const withBundleAnalyzer = require('@next/bundle-analyzer')({ enabled: false });
 const withSass = require('@zeit/next-sass');
 
 const isDev = process.env.NODE_ENV !== 'production';
 
-module.exports = withBundleAnalyzer(withPWA({
-  distDir: '_next',
-  pwa: {
-    disable: process.env.NEXT_PUBLIC_IS_DEV === 'true',
-    dest: 'pwa',
-  },
-  typescript: {
-    // ignoreBuildErrors: true,
-  },
-  webpack(config, options) {
-    const { dev, isServer } = options;
+module.exports = withPlugins([
+  [
+    withBundleAnalyzer,
+    withPWA({
+      distDir: '_next',
+      pwa: {
+        disable: process.env.NEXT_PUBLIC_IS_DEV === 'true',
+        dest: 'pwa',
+      },
+      typescript: {
+        // ignoreBuildErrors: true,
+      },
+      webpack(config, options) {
+        const { dev, isServer } = options;
 
-    if (dev && isServer) {
-      config.plugins.push(new ForkTsCheckerWebpackPlugin({
-        logger: {
-          infrastructure: 'console',
-        },
-        eslint: {
-          enabled: true,
-          files: './src/**/*.{js,jsx,ts,tsx}'
-        },
-      }));
-    }
+        if (dev && isServer) {
+          config.plugins.push(
+            new ForkTsCheckerWebpackPlugin({
+              logger: {
+                infrastructure: 'console',
+              },
+              eslint: {
+                enabled: true,
+                files: './src/**/*.{js,jsx,ts,tsx}',
+              },
+            }),
+          );
+        }
+        return config;
+      },
+    }),
+  ],
+  [
+    withSass,
+    {
+      cssModules: true,
+    },
+  ],
+]);
 
-    return config;
-  },
-}));
+// module.exports = withBundleAnalyzer(withPWA({
+//   distDir: '_next',
+//   pwa: {
+//     disable: process.env.NEXT_PUBLIC_IS_DEV === 'true',
+//     dest: 'pwa',
+//   },
+//   typescript: {
+//     // ignoreBuildErrors: true,
+//   },
+//   webpack(config, options) {
+//     const { dev, isServer } = options;
 
-module.exports = withSass({
-  cssModules: true,
-});
+//     if (dev && isServer) {
+//       config.plugins.push(new ForkTsCheckerWebpackPlugin({
+//         logger: {
+//           infrastructure: 'console',
+//         },
+//         eslint: {
+//           enabled: true,
+//           files: './src/**/*.{js,jsx,ts,tsx}'
+//         },
+//       }));
+//     }
+
+//     return config;
+//   },
+// }));
