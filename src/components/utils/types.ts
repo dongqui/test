39213d959158e9c7054a427972600c8a02387638
0xbono <iam@sonjim.in>
2021-02-01 08:13:34 +0900

    chore(types): 기존 SizeType에서 xl ~ xxl 추가

--- src/components/utils/types.ts
+++ src/components/utils/types.ts
@@ -1,3 +1,3 @@
-export type SizeType = 'small' | 'middle' | 'large' | undefined;
+export type SizeType = 'sm' | 'md' | 'lg' | 'xl' | 'xxl';
 export type Omit<T, K extends keyof T> = Pick<T, Exclude<keyof T, K>>;
 export type UnionType<T extends U, U> = T | (U & {});
