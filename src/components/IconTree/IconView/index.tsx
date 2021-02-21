97a7443cd325ccd9558666038012fdbbdf8ffb61
이자훈 <leejahun0@gmail.com>
2021-02-21 20:21:44 +0900

    Feature/jahun (#83) (#84)
    * Develop (#81)
    
    * fbx upload (#78)
    
    * Feature/jahun (#80)
    
    * fbx upload
    
    * drag and drop change logic
    
    * rp ondrop add
    
    * Update index.tsx

--- src/components/IconTree/IconView/index.tsx
+++ src/components/IconTree/IconView/index.tsx
@@ -27,7 +27,6 @@ const IconViewComponent: React.FC<IconViewProps> = ({
   const pages = useReactiveVar(PAGES);
   const searchWord = useReactiveVar(SEARCH_WORD);
   const contextmenuInfo = useReactiveVar(CONTEXTMENU_INFO);
-  // const [isDraggingItemKeys, setIsDraggingItemKeys] = useState<string[]>([]);
   const iconViewWrapperRef = useRef<HTMLDivElement | any>(null);
   const filteredData: mainDataTypes[] = useMemo(() => {
     let result = _.filter(mainData, (o) => _.isEqual(o.parentKey, _.last(pages)?.key));
