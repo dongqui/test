0fb854148d4371d761d823d656e9d76a9ca11b23
이자훈 <leejahun0@gmail.com>
2021-03-13 14:49:05 +0900

    test

--- src/containers/CutEdit/CutImages.tsx
+++ src/containers/CutEdit/CutImages.tsx
@@ -2,7 +2,6 @@ import { useReactiveVar } from '@apollo/client';
 import { CUT_IMAGES } from 'lib/store';
 import _ from 'lodash';
 import React from 'react';
-import { CUT_IMAGES_CNT } from 'utils/const';
 import * as S from './CutEdit.styles';
 
 export interface CutImagesProps {}
@@ -10,11 +9,11 @@ export interface CutImagesProps {}
 const CutImagesComponent: React.FC<CutImagesProps> = ({}) => {
   const cutImages = useReactiveVar(CUT_IMAGES);
   return (
-    <>
-      {_.map(Array(CUT_IMAGES_CNT), (item, index) => (
-        <S.CutImage draggable={false} key={index} src={cutImages?.[index]} />
+    <S.CutImagesWrapper>
+      {_.map(cutImages, (item, index) => (
+        <S.CutImage draggable={false} key={index} src={item} />
       ))}
-    </>
+    </S.CutImagesWrapper>
   );
 };
 export const CutImages = React.memo(CutImagesComponent);
