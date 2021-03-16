a2f6079c73a58860b22c01430b7e6aa44bbb6c89
CharmingCheol <toby0806@naver.com>
2021-03-16 16:54:10 +0900

    타임 프레임 위치 고정

--- src/containers/panels/timeline/TimeFrameView/DopeSheet/index.tsx
+++ src/containers/panels/timeline/TimeFrameView/DopeSheet/index.tsx
@@ -13,40 +13,25 @@ interface Datum {
 }
 
 const CIRCLE_RADIUS = 12; // 원 반지름 크기
-const TRACK_HEIGHT = 44.2601; // 트랙 높이
+const TRACK_HEIGHT = 48; // 트랙 높이
 const DOPE_SHEET_MARGIN = { top: 8, right: 20, bottom: 30, left: 30 }; // dope sheet에 적용 된 margin
-const HORIZONTAL_SCROLL_BAR_HEIGHT = 8; // dioe sheet 가로 스크롤 바 높이
-const dummyData = [
-  { time: 1, track: 1 },
-  { time: 2, track: 1 },
-  { time: 3, track: 1 },
-  { time: 1, track: 2 },
-  { time: 2, track: 2 },
-  { time: 3, track: 2 },
-  { time: 1, track: 3 },
-  { time: 2, track: 3 },
-  { time: 3, track: 3 },
-  { time: 1, track: 4 },
-  { time: 2, track: 4 },
-  { time: 3, track: 4 },
-  { time: 1, track: 5 },
-  { time: 2, track: 5 },
-  { time: 3, track: 5 },
-  { time: 1, track: 6 },
-  { time: 2, track: 6 },
-  { time: 3, track: 6 },
-];
+const dummyData = Array(1200) // 더미데이터
+  .fill(0)
+  .map((_, index) => {
+    return { track: Math.floor(index / 100) + 1, time: (index % 100) + 1 };
+  });
 const cx = classNames.bind(styles);
 
 const DopeSheet: React.FC<Props> = () => {
   const dopeSheetRef = useRef<HTMLDivElement>(null);
   const [zoomTransform, setZoomTransform] = useState<d3.ZoomTransform>();
   const circleY = useMemo(
-    () => (columnIndex: number) => TRACK_HEIGHT * columnIndex + TRACK_HEIGHT / 2 - CIRCLE_RADIUS,
+    () => (columnIndex: number) =>
+      TRACK_HEIGHT * columnIndex + TRACK_HEIGHT / 2 + CIRCLE_RADIUS / 2,
     [],
   );
 
-  // dope sheet 생성, zoom 적용
+  // dope sheet 초기 세팅
   useEffect(() => {
     if (!dopeSheetRef.current) return;
     const { clientWidth: width, clientHeight: height } = dopeSheetRef.current;
@@ -60,21 +45,16 @@ const DopeSheet: React.FC<Props> = () => {
     // x축 그래프 생성
     const xAxis = (g: any) =>
       g
-        .attr('transform', `translate(${DOPE_SHEET_MARGIN.left},${DOPE_SHEET_MARGIN.top})`)
-        .call(d3.axisTop(x).ticks(width / 80))
+        .call(d3.axisTop(x).ticks(width / 50))
         .attr('class', 'grid')
+        .style('position', 'fixed')
+        .style('z-index', 1)
+        .style('background', '#151515')
+        .style('border', '1px solid #393939')
         .call(createGridLineX());
 
     // x축 그래프 눈금 적용
-    const createGridLineX = () => {
-      return d3.axisBottom(x).ticks(width / 50);
-    };
-
-    // x축 그래프 다시 그리기
-    if (zoomTransform) {
-      const newXScale = zoomTransform.rescaleX(x);
-      x.domain(newXScale.domain());
-    }
+    const createGridLineX = () => d3.axisBottom(x).ticks(width / 50);
 
     // zoom 이벤트 적용
     const zoomBehavior: any = d3
@@ -88,53 +68,102 @@ const DopeSheet: React.FC<Props> = () => {
         setZoomTransform(event.transform);
       });
 
-    // svg 생성
-    const svg = d3
+    // dope sheet svg 생성
+    const dopeSheet = d3
       .select(dopeSheetRef.current)
-      .call((g) => g.select('svg').remove())
       .append('svg')
-      .attr('width', '100%')
-      .attr('class', 'dopesheet_svg')
-      .attr('height', TRACK_HEIGHT * dummyData.length);
+      .attr('class', 'dopesheet-svg')
+      .attr('width', width)
+      .attr('height', TRACK_HEIGHT * 12)
+      .attr('transform', `translate(0, ${TRACK_HEIGHT})`);
     d3.select(dopeSheetRef.current).call(zoomBehavior);
 
-    // 생성 된 x축 그래프 랜더링
-    svg.append('g').call(xAxis);
-
     // circle group 생성
-    const circleGroup = svg
+    const circleGroup = dopeSheet
       .append('g')
-      .attr('class', 'circle_group')
-      .attr('fill', 'none')
-      .attr('stroke-linecap', 'round');
+      .attr('class', 'circle-group')
+      .attr('height', TRACK_HEIGHT * 12);
+
+    // circle group을 rect로 생성
+    circleGroup
+      .selectAll('rect')
+      .data(Array(13).fill(0))
+      .join('rect')
+      .attr('width', width)
+      .attr('height', TRACK_HEIGHT)
+      .attr('fill', '#151515')
+      .attr('stroke-width', 1)
+      .attr('stroke', 'rgb(57, 57, 57)')
+      .attr('transform', (_, index) => `translate(0, ${TRACK_HEIGHT * index})`);
 
     // circle 생성
     circleGroup
       .selectAll('circle')
       .data(dummyData)
       .join('circle')
-      .attr('cx', (data) => x(data.time) + CIRCLE_RADIUS * 2.5)
-      .attr('cy', (data) => circleY(data.track) - DOPE_SHEET_MARGIN.top)
+      .attr('cx', (data) => x(data.time - 1))
+      // .attr('cy', (data) => 70)
+      .attr('cy', (data) => circleY(data.track - 1) - DOPE_SHEET_MARGIN.top)
       .attr('r', CIRCLE_RADIUS)
       .attr('stroke', '#ffffff');
 
-    // 하단 스크롤 바 생성
-    svg
-      .append('rect')
-      .attr('width', width - DOPE_SHEET_MARGIN.left)
-      .attr('height', HORIZONTAL_SCROLL_BAR_HEIGHT)
-      .attr('rx', HORIZONTAL_SCROLL_BAR_HEIGHT / 2)
-      .attr('ry', HORIZONTAL_SCROLL_BAR_HEIGHT / 2)
-      .attr('fill', '#454545')
-      .attr(
-        'transform',
-        `translate(${DOPE_SHEET_MARGIN.left},${TRACK_HEIGHT * 5 + HORIZONTAL_SCROLL_BAR_HEIGHT})`,
-      );
-  }, [circleY, zoomTransform]);
+    // x축 그래프 svg 생성
+    d3.select(dopeSheetRef.current)
+      .append('svg')
+      .attr('width', '100%')
+      .attr('height', TRACK_HEIGHT)
+      .call(xAxis);
+  }, [circleY]);
+
+  // zoom in/out, 좌우 Pad 발생 시 circle x값, x축 눈금 치수 변경
+  useEffect(() => {
+    if (!dopeSheetRef.current || !zoomTransform) return;
+    const { clientWidth: width } = dopeSheetRef.current;
+
+    // x 좌표 범위 설정
+    const x = d3
+      .scaleLinear()
+      .domain([-500000, 500000])
+      .range([DOPE_SHEET_MARGIN.left, width - DOPE_SHEET_MARGIN.right]);
+
+    // x축 그래프 생성
+    const xAxis = (g: any) =>
+      g
+        .call(d3.axisTop(x).ticks(width / 50))
+        .attr('class', 'grid')
+        .style('position', 'fixed')
+        .style('z-index', 1)
+        .style('background', '#151515')
+        .style('border', '1px solid #393939')
+        .call(createGridLineX());
+
+    // x축 그래프 눈금 적용
+    const createGridLineX = () => d3.axisBottom(x).ticks(width / 50);
+
+    // x축 비율 조정
+    const newXScale = zoomTransform.rescaleX(x);
+    x.domain(newXScale.domain());
+
+    // 기존 x축 그래프 지우기
+    d3.select('.grid').remove();
+    d3.select(dopeSheetRef.current)
+      .append('svg')
+      .attr('width', '100%')
+      .attr('height', TRACK_HEIGHT)
+      .call(xAxis);
+
+    // circle x 위치 조정
+    const circleGroup = d3.select('.circle-group');
+    circleGroup.selectAll('circle').attr('cx', (value: any) => x(value.time) + CIRCLE_RADIUS * 2.5);
+  }, [zoomTransform]);
 
   return (
     <>
-      <div className={cx('dopesheet-container')} ref={dopeSheetRef} />
+      <div
+        className={cx('dopesheet-container')}
+        ref={dopeSheetRef}
+        style={{ height: TRACK_HEIGHT * 12 }}
+      />
     </>
   );
 };
