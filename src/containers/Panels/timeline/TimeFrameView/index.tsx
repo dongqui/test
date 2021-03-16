5a24f58d535d7c052b46ddc2a1dcc3a6dfcef365
CharmingCheol <toby0806@naver.com>
2021-03-16 18:26:14 +0900

    timeline 폴더 추가

---
+++ src/containers/Panels/timeline/TimeFrameView/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import DopeSheet from './DopeSheet';
+
+interface Props {}
+
+const TimeFrameView: React.FC<Props> = () => {
+  return <DopeSheet />;
+};
+
+export default TimeFrameView;
