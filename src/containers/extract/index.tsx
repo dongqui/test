d5678f17be7d485681a422a1050b5b31d100935d
이자훈 <leejahun0@gmail.com>
2021-03-18 02:30:12 +0900

    InputCP component 작업중

--- src/containers/extract/index.tsx
+++ src/containers/extract/index.tsx
@@ -1,4 +1,11 @@
-import React, { ChangeEvent, useCallback, useEffect, useRef, useState } from 'react';
+import React, {
+  ChangeEvent,
+  useCallback,
+  useEffect,
+  useLayoutEffect,
+  useRef,
+  useState,
+} from 'react';
 import { NextPage } from 'next';
 import _ from 'lodash';
 import { v4 as uuidv4 } from 'uuid';
--- src/docs/Input/InputCP.stories.tsx
+++ src/docs/Input/InputCP.stories.tsx
@@ -1,35 +1,16 @@
 import React from 'react';
 import '../common.css';
 import { Story, Meta } from '@storybook/react/types-6-0';
-import {
-  Input,
-  InputChildProps,
-  InputProps,
-  InputWrapperProps,
-} from '../../components/Input/InputCP';
+import { Input, InputCPProps } from '../../components/Input/InputCP';
 
 export default {
   title: 'Component API/Component/Input/InputCP',
   component: Input,
-  argTypes: {
-    backgroundColor: {
-      control: {
-        type: 'color',
-      },
-    },
-  },
+  argTypes: {},
   args: {},
 } as Meta;
 
-const Template: Story<InputProps & InputWrapperProps & InputChildProps> = (args) => (
-  <Input {...args} />
-);
+const Template: Story<InputCPProps> = (args) => <Input {...args} />;
 
 export const Default = Template.bind({});
-Default.args = {
-  width: '5rem',
-  height: '2.5rem',
-  backgroundColor: `var(--gray400)`,
-  borderRadius: 4,
-  prefix: 'X',
-};
+Default.args = {};
