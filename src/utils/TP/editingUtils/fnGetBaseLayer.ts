1b48a2eccaa5459c710646daf80524f3acdb892e
이자훈 <leejahun0@gmail.com>
2021-03-19 00:12:32 +0900

    Update fnGetBaseLayer.ts

--- src/utils/TP/editingUtils/fnGetBaseLayer.ts
+++ src/utils/TP/editingUtils/fnGetBaseLayer.ts
@@ -1,6 +1,6 @@
 import * as THREE from 'three';
 import _ from 'lodash';
-import fnQuaternionToEulerTrack from 'utils/common/FnQuaternionToEulerTrack';
+import fnQuaternionToEulerTrack from 'utils/common/fnQuaternionToEulerTrack';
 import { ShootTrackType } from 'types/common';
 
 interface FnGetBaseLayer {
