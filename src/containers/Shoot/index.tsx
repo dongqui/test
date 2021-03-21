495b0e31d70fb2d120a814d75e28cda89400bf86
jgpark <jjtjs159@gmail.com>
2021-03-21 19:09:13 +0900

    Shoot container uppercase로 수정

---
+++ src/containers/Shoot/index.tsx
@@ -0,0 +1,81 @@
+import React, { useEffect, useRef } from 'react';
+import { NextPage } from 'next';
+import _ from 'lodash';
+import styled from '@emotion/styled';
+import { useReactiveVar } from '@apollo/client';
+import { Contextmenu } from 'components/Contextmenu';
+import MainPage from './MainPage';
+import { useOutsideClick } from 'hooks/common/useOutsideClick';
+import { CONTEXTMENU_INFO, MODAL_INFO } from 'lib/store';
+import { GRAY200 } from 'styles/constants/common';
+import { Modal } from 'components/Modal';
+import { MODAL_TYPES } from 'types';
+import { fnKillSetInterval } from 'utils/common/fnKillSetInterval';
+import { BaseModal } from 'components/New_Modal';
+import { ModalInner } from 'docs/New_components/Modal/BaseModal.stories';
+interface ContextMenuWrapperProps {
+  top: string;
+  left: string;
+}
+interface ModalWrapperProps {
+  active: boolean;
+}
+
+const ContextMenuWrapper = styled.div<ContextMenuWrapperProps>`
+  position: absolute;
+  top: ${(props) => props.top};
+  left: ${(props) => props.left};
+  z-index: 1000;
+`;
+
+interface Props {}
+
+const ShootPage: NextPage<Props> = () => {
+  const contextMenuInfo = useReactiveVar(CONTEXTMENU_INFO);
+  const modalInfo = useReactiveVar(MODAL_INFO);
+  const contextMenuRef = useRef<HTMLDivElement | any>(null);
+  const modalRef = useRef<HTMLDivElement | any>(null);
+
+  useOutsideClick({
+    ref: contextMenuRef,
+    event: () => {
+      CONTEXTMENU_INFO({ ...contextMenuInfo, isShow: false });
+      MODAL_INFO({ ...modalInfo, isShow: false, msg: '' });
+    },
+  });
+  useEffect(() => {
+    fnKillSetInterval();
+  }, []);
+
+  return (
+    <main>
+      {contextMenuInfo.isShow && (
+        <ContextMenuWrapper
+          ref={contextMenuRef}
+          top={`${contextMenuInfo.top}px`}
+          left={`${contextMenuInfo.left}px`}
+        >
+          <Contextmenu
+            width="8rem"
+            height="3rem"
+            backgroundColor={GRAY200}
+            data={contextMenuInfo.data}
+            onClick={contextMenuInfo.onClick}
+          />
+        </ContextMenuWrapper>
+      )}
+      {modalInfo.isShow && (
+        <>
+          {_.isEqual(modalInfo.type, MODAL_TYPES.alert) && (
+            <BaseModal hasCloseIcon onClose={() => {}} theme="dark">
+              <ModalInner>{modalInfo.msg}</ModalInner>
+            </BaseModal>
+          )}
+        </>
+      )}
+      <MainPage />
+    </main>
+  );
+};
+
+export default ShootPage;
--- src/pages/shoot.tsx
+++ src/pages/shoot.tsx
@@ -1,5 +1,5 @@
 import dynamic from 'next/dynamic';
 
-const DynamicWithNoSSR = dynamic(() => import('containers/shoot'), { ssr: false });
+const DynamicWithNoSSR = dynamic(() => import('containers/Shoot'), { ssr: false });
 
 export default DynamicWithNoSSR;
