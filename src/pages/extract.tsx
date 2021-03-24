80c98673986178401c725c40fdabac4ece2178e8
이자훈 <leejahun0@gmail.com>
2021-03-24 14:38:41 +0900

    임시 커밋

--- src/pages/extract.tsx
+++ src/pages/extract.tsx
@@ -1,5 +1,5 @@
 import dynamic from 'next/dynamic';
 
-const DynamicWithNoSSR = dynamic(() => import('containers/extract'), { ssr: true });
+const DynamicWithNoSSR = dynamic(() => import('containers/extract'), { ssr: false });
 
 export default DynamicWithNoSSR;
