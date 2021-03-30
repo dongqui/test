f8dbbb0b00a6e00f90cfc9e831ea5f3ef11da591
0xbono <iam@sonjim.in>
2021-03-30 10:23:15 +0900

    컨트롤 패널 기본 레이아웃 추가

--- src/docs/Panels/ControlPanel/ControlPanel.stories.tsx
+++ src/docs/Panels/ControlPanel/ControlPanel.stories.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import '../../common.css';
 // also exported from '@storybook/react' if you can deal with breaking changes in 6.1
 import { Story, Meta } from '@storybook/react/types-6-0';
-import { ControlPanel, ControlPanelProps } from 'containers/Panels/ControlPanel';
+import { ControlPanel, ControlPanelProps } from 'containers/Panels/ControlPanel/old';
 
 export default {
   title: 'Panels/ControlPanel',
