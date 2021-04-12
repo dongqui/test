a291b5e9a432bfce07e7ef8f33222293c441825b
CharmingCheol <toby0806@naver.com>
2021-04-12 16:06:37 +0900

    트랙 잠금 효과 utils 추가

--- src/utils/TP/trackUtils/index.ts
+++ src/utils/TP/trackUtils/index.ts
@@ -1,7 +1,12 @@
 export { default as fnCheckSelectedTrackList } from './fnCheckSelectedTrackList';
 
+export { default as fnClickLockButton } from './fnClickLockButton';
 export { default as fnClickTrackToCtrlKey } from './fnClickTrackToCtrlKey';
 export { default as fnClickTrackToMouse } from './fnClickTrackToMouse';
+
+export { default as fnLockBoneTrack } from './fnLockBoneTrack';
+export { default as fnLockLayerTrack } from './fnLockLayerTrack';
+export { default as fnLockTransformTrack } from './fnLockTransformTrack';
 
 export { default as fnSelectBoneTrack } from './fnSelectBoneTrack';
 export { default as fnSelectLayerTrack } from './fnSelectLayerTrack';
