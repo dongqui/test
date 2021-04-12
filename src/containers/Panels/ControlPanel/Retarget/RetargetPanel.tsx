b5074d9151095512c61434b9f745f69e6ad8f663
0xbono <iam@sonjim.in>
2021-04-12 21:14:45 +0900

    비쥬얼 전엔 리타게팅 패널에 접근 비활성화 및 리타게팅 패널 추가

--- src/containers/Panels/ControlPanel/Retarget/RetargetPanel.tsx
+++ src/containers/Panels/ControlPanel/Retarget/RetargetPanel.tsx
@@ -1,48 +1,83 @@
 import { FunctionComponent } from 'react';
 import { Dropdown } from 'components/New_Dropdown';
-import { PrefixInput } from 'components/New_Input';
+import { SuffixInput } from 'components/New_Input';
 import { IconWrapper, SvgPath } from 'components/New_Icon';
 import classNames from 'classnames/bind';
 import styles from './RetargetPanel.module.scss';
 
 const cx = classNames.bind(styles);
 
-const RetargetPanel: FunctionComponent<{}> = () => {
+interface BaseProps {
+  // error: boolean;
+}
+
+export type P = BaseProps;
+
+const RetargetPanel: FunctionComponent<P> = ({}) => {
   const fasterList = [
     {
-      key: '0.25',
+      key: 'Default',
       value: 'Target Bone',
       isSelected: true,
     },
   ];
   const coordList = [
     {
-      key: '0.25',
-      value: 'X',
+      key: 'XYZ',
+      value: 'XYZ',
       isSelected: true,
+    },
+    {
+      key: 'YZX',
+      value: 'YZX',
+      isSelected: false,
+    },
+    {
+      key: 'ZXY',
+      value: 'ZXY',
+      isSelected: false,
+    },
+    {
+      key: 'ZYX',
+      value: 'ZYX',
+      isSelected: false,
+    },
+    {
+      key: 'YZX',
+      value: 'YZX',
+      isSelected: false,
+    },
+    {
+      key: 'YXZ',
+      value: 'YXZ',
+      isSelected: false,
     },
   ];
 
   const handleFasterSelect = () => {};
 
+  const handleRetargetRefresh = () => {
+    alert('Refresh');
+  };
+
   return (
     <main className={cx('panel-wrap')}>
-      {/* <section className={cx('section-setup')}>
-        <div className={cx('setup-group')}>
+      <section className={cx('section-setup')}>
+        <ul className={cx('setup-group')}>
+          <IconWrapper
+            className={cx('icon')}
+            icon={SvgPath.Refresh}
+            onClick={handleRetargetRefresh}
+            hasFrame={false}
+          />
           <IconWrapper
             className={cx('icon')}
             icon={SvgPath.Error}
             onClick={() => {}}
             hasFrame={false}
           />
-          <IconWrapper
-            className={cx('icon')}
-            icon={SvgPath.Refresh}
-            onClick={() => {}}
-            hasFrame={false}
-          />
-        </div>
-      </section> */}
+        </ul>
+      </section>
       <section className={cx('section-retarget')}>
         <div className={cx('retarget-card')}>
           <div className={cx('card-header')}>
@@ -51,16 +86,18 @@ const RetargetPanel: FunctionComponent<{}> = () => {
           </div>
           <div className={cx('card-coord')}>
             <Dropdown list={coordList} onSelect={handleFasterSelect} />
-            <PrefixInput prefix="X" />
-            <Dropdown list={coordList} onSelect={handleFasterSelect} />
-            <PrefixInput prefix="X" />
-            <Dropdown list={coordList} onSelect={handleFasterSelect} />
-            <PrefixInput prefix="X" />
+            <div className={cx('card-input-group')}>
+              <SuffixInput suffix="°" />
+              <SuffixInput suffix="°" />
+              <SuffixInput suffix="°" />
+            </div>
           </div>
         </div>
       </section>
     </main>
   );
 };
+
+// °
 
 export default RetargetPanel;
