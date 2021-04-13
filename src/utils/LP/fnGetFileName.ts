3a2ca1a4540d48d1c91ad6878a7ddc5b3534f7c5
이자훈 <leejahun0@gmail.com>
2021-04-13 17:16:22 +0900

    이름중복체크함수 수정

--- src/utils/LP/fnGetFileName.ts
+++ src/utils/LP/fnGetFileName.ts
@@ -1,14 +1,18 @@
 import _ from 'lodash';
-import { LPDataType } from 'types';
+import { LPDataType, LPDATA_PROPERTY_TYPES } from 'types';
 
 interface fnGetFileNameProps {
   key: string;
   name: string;
   mainData: LPDataType[];
+  parentKey?: string;
 }
 
-export const fnGetFileName = ({ key, name, mainData }: fnGetFileNameProps) => {
-  const filteredMainData = _.filter(mainData, (item) => !_.isEqual(item.key, key));
+export const fnGetFileName = ({ key, name, mainData, parentKey }: fnGetFileNameProps) => {
+  let filteredMainData = _.filter(mainData, (item) => !_.isEqual(item.key, key));
+  if (parentKey) {
+    filteredMainData = _.filter(mainData, [LPDATA_PROPERTY_TYPES.parentKey, parentKey]);
+  }
   if (_.some(filteredMainData, (item) => _.isEqual(item.name, name))) {
     return `${name} (${
       _.size(_.filter(filteredMainData, (item) => _.includes(item.name, name))) + 1
