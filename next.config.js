866fc7a853245cb9e9f3447c97d1081c3f648889
jgpark <jjtjs159@gmail.com>
2021-04-16 01:21:52 +0900

    moment 번들 사이즈 축소를 위한 설정 추가 및 중복 라이브러리 체커 추가, nextpwa 디버그 콘솔 비활성

--- next.config.js
+++ next.config.js
@@ -1,14 +1,17 @@
-const withPWA = require('next-pwa');
+// const withPWA = require('next-pwa');
+const webpack = require('webpack');
 const path = require('path');
 const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin');
+// 증복된 라이브러리를 체크하기 위한 checker (빌드 시에만 활성화시켜 확인)
+// const DuplicatePackageCheckerPlugin = require('duplicate-package-checker-webpack-plugin');
 const withBundleAnalyzer = require('@next/bundle-analyzer')({ enabled: false });
 
-module.exports = withBundleAnalyzer(withPWA({
+module.exports = withBundleAnalyzer({
   distDir: '_next',
-  pwa: {
-    disable: true,
-    dest: 'pwa',
-  },
+  // pwa: {
+  //   disable: true,
+  //   dest: 'pwa',
+  // },
   typescript: {
     ignoreBuildErrors: true,
   },
@@ -31,6 +34,21 @@ module.exports = withBundleAnalyzer(withPWA({
         }),
       );
     }
+
+    /**
+     * 번들사이즈를 줄이기 위해
+     * moment의 locales 중 필요한 언어만 가져오기 위한 설정
+     */
+    config.plugins.push(
+      new webpack.ContextReplacementPlugin(
+        /moment[/\\]locale$/,
+        /ko/,
+      ),
+    );
+
+    // 증복된 라이브러리를 체크하기 위한 checker
+    // config.plugins.push(new DuplicatePackageCheckerPlugin());
+
     return config;
   },
-}));
+});
