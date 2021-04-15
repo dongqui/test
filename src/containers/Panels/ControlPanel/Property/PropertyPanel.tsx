e5f6482835c1a9bd23a2bb1e7ba3171e1db4abed
0xbono <iam@sonjim.in>
2021-04-15 11:19:38 +0900

    컨트롤패널 이슈 해결 및 코드 적용

--- src/containers/Panels/ControlPanel/Property/PropertyPanel.tsx
+++ src/containers/Panels/ControlPanel/Property/PropertyPanel.tsx
@@ -1,65 +1,58 @@
-import { FunctionComponent } from 'react';
-import { Segment } from 'components/New_Segment';
-import { PrefixInput } from 'components/New_Input';
+/* eslint-disable prefer-const */
+import { FunctionComponent, Fragment } from 'react';
+import { useReactiveVar } from '@apollo/client';
 import { AccordionMenu } from 'components/New_AccordionMenu';
+import { storeCPData } from 'lib/store';
+import _ from 'lodash';
 import classNames from 'classnames/bind';
 import styles from './PropertyPanel.module.scss';
+import { CPComponentType, CPDataPropertyNames } from 'types/CP';
+import { CPListRowInput } from './CPListTree/CPListRowInput';
+import { CPListRowButton } from './CPListTree/CPListRowButton';
 
 const cx = classNames.bind(styles);
 
-const PropertyPanel: FunctionComponent<{}> = () => {
-  const objList = [
-    { id: 1, contents: 'ON' },
-    { id: 2, contents: 'OFF' },
-  ];
+const PropertyPanel: FunctionComponent<{}> = ({}) => {
+  const cpData = useReactiveVar(storeCPData);
+
   return (
     <main className={cx('panel-wrap')}>
       <section className={cx('panel-transform')}>
         <AccordionMenu title="Transform">
-          <div className={cx('transform-inner')}>
-            <span className={cx('property-title')}>Position</span>
-            <div className={cx('transform-group')}>
-              <PrefixInput prefix="X" />
-              <PrefixInput prefix="Y" />
-              <PrefixInput prefix="Z" />
-            </div>
-          </div>
-          <div className={cx('transform-inner')}>
-            <span className={cx('property-title')}>Rotation</span>
-            <div className={cx('transform-group')}>
-              <PrefixInput prefix="X" />
-              <PrefixInput prefix="Y" />
-              <PrefixInput prefix="Z" />
-            </div>
-          </div>
-          <div className={cx('transform-inner')}>
-            <span className={cx('property-title')}>Scale</span>
-            <div className={cx('transform-group')}>
-              <PrefixInput prefix="X" />
-              <PrefixInput prefix="Y" />
-              <PrefixInput prefix="Z" />
-            </div>
-          </div>
+          {_.map(cpData, (item, idx) => (
+            <Fragment>
+              <div className={cx('transform-group')}>
+                {_.isEqual(item.type, CPComponentType.input) &&
+                  _.find(cpData, [CPDataPropertyNames.key, item?.parentKey])?.isExpanded && (
+                    <Fragment key={idx}>
+                      <CPListRowInput
+                        rowKey={item.key}
+                        name={item.name}
+                        x={item.x}
+                        y={item.y}
+                        z={item.z}
+                      />
+                    </Fragment>
+                  )}
+              </div>
+            </Fragment>
+          ))}
         </AccordionMenu>
       </section>
       <section className={cx('panel-visibility')}>
         <AccordionMenu title="Visibility">
-          <div className={cx('segment-group')}>
-            <span>Axis</span>
-            <Segment objList={objList} />
-          </div>
-          <div className={cx('segment-group')}>
-            <span>Bone</span>
-            <Segment objList={objList} />
-          </div>
-          <div className={cx('segment-group')}>
-            <span>Mesh</span>
-            <Segment objList={objList} />
-          </div>
-          <div className={cx('segment-group')}>
-            <span>Shadow</span>
-            <Segment objList={objList} />
-          </div>
+          {_.map(cpData, (item, idx) => (
+            <Fragment>
+              <div className={cx('transform-group')}>
+                {_.isEqual(item.type, CPComponentType.select) &&
+                  _.find(cpData, [CPDataPropertyNames.key, item?.parentKey])?.isExpanded && (
+                    <Fragment key={idx}>
+                      <CPListRowButton rowKey={item.key} name={item.name} button={item.button} />
+                    </Fragment>
+                  )}
+              </div>
+            </Fragment>
+          ))}
         </AccordionMenu>
       </section>
     </main>
