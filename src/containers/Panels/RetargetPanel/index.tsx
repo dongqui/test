9867794c4f57c7effab31480a3df34a9a7f37b3a
0xbono <iam@sonjim.in>
2021-04-19 13:57:51 +0900

    리타게팅 패널 order 이슈 제외 추가

--- src/containers/Panels/RetargetPanel/index.tsx
+++ src/containers/Panels/RetargetPanel/index.tsx
@@ -1,29 +1,29 @@
 import { useReactiveVar } from '@apollo/client';
-import { CPTitle } from 'containers/CPListTree/CPTitle';
-import { storeRetargetData } from 'lib/store';
-import _ from 'lodash';
-import React from 'react';
-import * as S from './RetargetPanelStyles';
-import { RetargetRow } from './RetargetRow';
+// import { CPTitle } from 'containers/CPListTree/CPTitle';
+// import { storeRetargetData } from 'lib/store';
+// import _ from 'lodash';
+// import React from 'react';
+// import * as S from './RetargetPanelStyles';
+// import { RetargetRow } from './RetargetRow';
 
-export interface RetargetPanelProps {
-  targetBones?: string[];
-}
-const RetargetPanelComponent: React.FC<RetargetPanelProps> = ({
-  targetBones = ['Source Bone1', 'Source Bone2', 'Source Bone3', 'Source Bone4', 'Source Bone5'],
-}) => {
-  const retargetData = useReactiveVar(storeRetargetData);
-  return (
-    <S.RetargetPanelWrapper>
-      <CPTitle title="Retarget" />
-      <S.PanelRetargetRowWrapper>
-        {_.map(retargetData, (item, index) => (
-          <S.PanelRowWrapper key={index}>
-            <RetargetRow boneName={item.boneName} index={index} targetBones={targetBones} />
-          </S.PanelRowWrapper>
-        ))}
-      </S.PanelRetargetRowWrapper>
-    </S.RetargetPanelWrapper>
-  );
-};
-export const RetargetPanel = React.memo(RetargetPanelComponent);
+// export interface RetargetPanelProps {
+//   targetBones?: string[];
+// }
+// const RetargetPanelComponent: React.FC<RetargetPanelProps> = ({
+//   targetBones = ['Source Bone1', 'Source Bone2', 'Source Bone3', 'Source Bone4', 'Source Bone5'],
+// }) => {
+//   const retargetData = useReactiveVar(storeRetargetData);
+//   return (
+//     <S.RetargetPanelWrapper>
+//       <CPTitle title="Retarget" />
+//       <S.PanelRetargetRowWrapper>
+//         {_.map(retargetData, (item, index) => (
+//           <S.PanelRowWrapper key={index}>
+//             <RetargetRow boneName={item.boneName} index={index} targetBones={targetBones} />
+//           </S.PanelRowWrapper>
+//         ))}
+//       </S.PanelRetargetRowWrapper>
+//     </S.RetargetPanelWrapper>
+//   );
+// };
+// export const RetargetPanel = React.memo(RetargetPanelComponent);
