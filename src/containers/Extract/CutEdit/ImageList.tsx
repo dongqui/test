889b7566165ac993a1e998ba3ebfd974b895de81
jgpark <jjtjs159@gmail.com>
2021-05-07 13:04:10 +0900

    대문자 Extract container 추가

---
+++ src/containers/Extract/CutEdit/ImageList.tsx
@@ -0,0 +1,35 @@
+import { FunctionComponent, memo } from 'react';
+import { useReactiveVar } from '@apollo/client';
+import { storeCutImages } from 'lib/store';
+import _ from 'lodash';
+import classNames from 'classnames/bind';
+import styles from './ImageList.module.scss';
+
+const cx = classNames.bind(styles);
+
+const ImageList: FunctionComponent = () => {
+  const images = useReactiveVar(storeCutImages);
+  const defaultList = Array.from(Array(20).keys());
+
+  return (
+    <div className={cx('wrapper')}>
+      {_.map(defaultList, (_cell, i) => (
+        <div className={cx('image-wrapper')} key={i}>
+          <div className={cx('image-inner')}>
+            {images[i] && (
+              <img
+                className={cx('image')}
+                draggable={false}
+                key={i}
+                src={images[i]}
+                alt="cut_image"
+              />
+            )}
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default memo(ImageList);
