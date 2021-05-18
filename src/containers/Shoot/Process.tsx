d5e230d04ac695df4c11e8a29c88a4e1daf94a4b
jgpark <jjtjs159@gmail.com>
2021-05-19 01:03:59 +0900

    Shoot process 컨테이너 세부 디자인 및 token api 관련 절차 로직 추가

--- src/containers/Shoot/Process.tsx
+++ src/containers/Shoot/Process.tsx
@@ -1,16 +1,69 @@
-import { FunctionComponent } from 'react';
+import { FunctionComponent, Fragment, useCallback } from 'react';
+import { useRouter } from 'next/router';
 import { Video } from 'components/Video';
+import { Overlay } from 'components/Overlay';
+import { Headline } from 'components/Typography';
+import { TextButton } from 'components/New_Button';
+import { Procedure } from 'containers/Shoot';
 import classNames from 'classnames/bind';
 import styles from './Process.module.scss';
 
 const cx = classNames.bind(styles);
 
-interface Props {}
+interface Props {
+  procedure: Procedure;
+}
 
-const Process: FunctionComponent<Props> = ({}) => {
+/**
+ *
+ * @todo 애니메이션 넣어야 함
+ * @see https://codepen.io/yemon/pen/pWoROm
+ */
+const Process: FunctionComponent<Props> = ({ procedure }) => {
+  const router = useRouter();
+  const classes = cx('text', 'uppercase', procedure);
+  const deniedClasses = cx('text', procedure);
+
+  const isDenied = procedure === 'denied';
+
+  const handleClick = useCallback(() => {
+    router.push('https://plask.ai');
+  }, [router]);
+
   return (
     <div className={cx('wrapper')}>
+      <div className={cx('inner')}>
+        {isDenied ? (
+          <Fragment>
+            <Headline className={cx('text')} level="3" align="center" margin bold>
+              Oops! Discovered
+            </Headline>
+            <Headline className={deniedClasses} level="3" align="center" margin bold>
+              Unusual approach
+            </Headline>
+            <div className={cx('bottom')}>
+              <p className={cx('paragraph')}>Do you want a normal approach?</p>
+              <TextButton className={cx('button')} onClick={handleClick}>
+                Let’s Go
+              </TextButton>
+            </div>
+          </Fragment>
+        ) : (
+          <Fragment>
+            <Headline className={cx('text')} level="3" align="center" margin bold>
+              NOW
+            </Headline>
+            <Headline className={classes} level="3" align="center" margin bold>
+              {procedure}
+            </Headline>
+            <Headline className={cx('text')} level="3" align="center" margin bold>
+              LOADING
+            </Headline>
+          </Fragment>
+        )}
+      </div>
       <Video src="/video/loadingVideo.mp4" autoPlay loop fullSize />
+      <Overlay theme="dark" />
     </div>
   );
 };
--- src/containers/Shoot/index.tsx
+++ src/containers/Shoot/index.tsx
@@ -2,37 +2,74 @@ import { FunctionComponent, memo, Fragment, useCallback, useEffect, useState, us
 import _ from 'lodash';
 import { useReactiveVar } from '@apollo/client';
 import { ContextMenu } from 'components/New_ContextMenu';
-import MainPage from './MainPage';
 import { useRouter } from 'next/router';
 import { useOutsideClick } from 'hooks/common/useOutsideClick';
 import { storeContextMenuInfo, storeModalInfo, storePageInfo } from 'lib/store';
 import { MODAL_TYPES, PAGE_NAMES } from 'types';
 import { BaseModal } from 'components/New_Modal';
+import { Headline } from 'components/Typography';
+import Html from 'components/Typography/Html';
+import axios from 'axios';
+import MainPage from './MainPage';
 import ExtractPage from 'containers/extract';
-import Html from 'components/New_Typography/Html';
 import Process from 'containers/Shoot/Process';
-import { Headline } from 'components/New_Typography';
+
+export type Procedure = 'service' | 'token' | 'success' | 'denied';
 
 const ShootPage: FunctionComponent = () => {
   const router = useRouter();
+  const { token } = router.query;
 
-  const [isAccessible, setIsAccessible] = useState(false);
+  const [procedure, setProcedure] = useState<Procedure>('service');
+  const [_message, setMessage] = useState('');
 
   useEffect(() => {
     const isTokenLoaded = localStorage.getItem('token');
 
+    const authToken = async (token: string) => {
+      await axios({
+        method: 'GET',
+        url: 'http://115.85.182.106/verify',
+        params: { token },
+      })
+        .then((response) => {
+          console.log(response);
+
+          localStorage.setItem('token', JSON.stringify(token));
+          setProcedure('success');
+        })
+        .catch((error) => {
+          const { message } = error;
+          setProcedure('denied');
+          setMessage(message);
+        });
+    };
+
     if (isTokenLoaded) {
-      setIsAccessible(true);
+      // 강제로 1초 딜레이를 줘서 애니메이션을 보여주기 위함
+      setTimeout(() => {
+        setProcedure('success');
+      }, 1000);
     }
 
     if (!isTokenLoaded) {
-      if (router.query.token) {
-        setIsAccessible(true);
+      if (token && typeof token === 'string') {
+        // 토근 인증 절차
+        // 1. API로 쿼리스트링에 있는 토큰을 인증
+        // 단. 그동안 1초는 반드시 delay 시키고 + api 통신 시간 만큼 process container 요청 패턴 로드
+        setTimeout(() => {
+          setProcedure('token');
+        }, 1000);
 
-        localStorage.setItem('token', JSON.stringify(router.query.token));
+        // 강제로 2초 딜레이를 줘서 동일 흐름에서 token 프로세스를 먼저 밟고 이후 api 통신
+        setTimeout(() => {
+          authToken(token);
+        }, 2000);
+        // 2. 인증 성공 시 로컬스토리지에 토큰 저장 후 shoot 페이지 로드
+        // 3. 인증 실패 시 process container의 실패 패턴 로드
       }
     }
-  }, [router.query.token]);
+  }, [token]);
 
   const contextMenuInfo = useReactiveVar(storeContextMenuInfo);
   const modalInfo = useReactiveVar(storeModalInfo);
@@ -64,10 +101,10 @@ const ShootPage: FunctionComponent = () => {
     }
   }, [contextMenuInfo.isShow]);
 
-  if (!isAccessible) {
+  if (procedure !== 'success') {
     return (
       <main>
-        <Process />
+        <Process procedure={procedure} />
       </main>
     );
   }
