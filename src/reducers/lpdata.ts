43f1560314ccadf27edf1078b2526f25d919c937
이자훈 <leejahun0@gmail.com>
2021-05-25 18:15:33 +0900

    Update lpdata.ts

--- src/reducers/lpdata.ts
+++ src/reducers/lpdata.ts
@@ -1,7 +1,22 @@
+import _ from 'lodash';
 import { LPDatasState, LPDataAction } from 'actions/lpdata';
 
 export const ROOT_KEY = 'root';
 const defaultState: LPDatasState = [];
+
+interface FindDeleteKeys {
+  data: LPDatasState;
+  keys: string[];
+}
+
+const findDeleteKeys = (params: FindDeleteKeys): string[] => {
+  const { data, keys } = params;
+  const relationalKeys = data
+    .filter((item) => keys.includes(item.parentKey))
+    .map((item) => item.key);
+  const deleteKeys = _.concat(keys, relationalKeys);
+  return deleteKeys;
+};
 
 export const lpdata = (state: LPDatasState = defaultState, action: LPDataAction): LPDatasState => {
   switch (action.type) {
@@ -9,7 +24,8 @@ export const lpdata = (state: LPDatasState = defaultState, action: LPDataAction)
       return [...state, ...action.payload];
     }
     case 'lpdata/DELETE_LPDATA': {
-      return state.filter((item) => !action.payload.includes(item.key));
+      const deleteKeys = findDeleteKeys({ data: state, keys: action.payload });
+      return state.filter((item) => !deleteKeys.includes(item.key));
     }
     default: {
       return state;
