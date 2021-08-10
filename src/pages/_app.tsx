6a4e89adcbcbe19385a520bbc8e224c65e57f04c
jjtjs159-jg <61176987+jjtjs159-jg@users.noreply.github.com>
2021-08-11 01:34:37 +0900

    refactor: app, document, style 코드 정리

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,18 +1,17 @@
+import _ from 'lodash';
 import { Fragment, useEffect } from 'react';
-import Head from 'next/head';
 import { NextComponentType } from 'next';
-import { Provider } from 'react-redux';
 import { AppContext, AppInitialProps, AppProps } from 'next/app';
 import { wrapper } from 'store';
 import { hotjar } from 'analytics';
+import Head from 'next/head';
 import 'styles/core.scss';
-import 'styles/timeline/_curve.scss';
-import _ from 'lodash';
 
 const App: NextComponentType<AppContext, AppInitialProps, AppProps> = ({
   Component,
   pageProps,
 }) => {
+
   useEffect(() => {
     hotjar.initialize();
   }, []);
@@ -21,10 +20,7 @@ const App: NextComponentType<AppContext, AppInitialProps, AppProps> = ({
     <Fragment>
       <Head>
         <title>shoot</title>
-        <meta
-          name="viewport"
-          content="width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=1,user-scalable=no"
-        />
+        <meta name="viewport" content="width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=1,user-scalable=no" />
       </Head>
       <Component {...pageProps} />
     </Fragment>
