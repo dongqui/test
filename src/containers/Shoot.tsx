5a0e8368694c814db354234d3735d127b0c81a12
jjtjs159-jg <61176987+jjtjs159-jg@users.noreply.github.com>
2021-08-12 13:49:58 +0900

    feat: shoot 메인 upper section, lower section y축 리사이즈 기능 추가

--- src/containers/Shoot.tsx
+++ src/containers/Shoot.tsx
@@ -1,4 +1,4 @@
-import { FunctionComponent, useState, useCallback } from 'react';
+import { FunctionComponent, Fragment, useState, useCallback } from 'react';
 import { ResizableBox, ResizeCallbackData } from 'react-resizable';
 import { useWindowSize } from 'hooks/common';
 import classNames from 'classnames/bind';
@@ -6,23 +6,19 @@ import styles from './Shoot.module.scss';
 
 const cx = classNames.bind(styles);
 
-interface Props {
-
-}
-
-const Shoot: FunctionComponent<Props> = () => {
+const Shoot: FunctionComponent = () => {
 
   const [windowWidth, windowHeight] = useWindowSize();
 
   const [sectionHeight, setSectionHeight] = useState({
-    upperSection: windowHeight * 0.7,
-    lowerSection: windowHeight * 0.3,
+    upperSection: windowHeight - 168 - 36,
+    lowerSection: 168,
   });
 
   const handleResize = useCallback(
     (_e: React.SyntheticEvent, data: ResizeCallbackData) => {
       setSectionHeight({
-        upperSection: windowHeight - data.size.height,
+        upperSection: windowHeight - data.size.height - 36,
         lowerSection: data.size.height,
       });
     },
@@ -35,26 +31,29 @@ const Shoot: FunctionComponent<Props> = () => {
       <ResizableBox
         width={windowWidth}
         height={sectionHeight.upperSection}
-        minConstraints={[windowWidth, windowHeight * 0.5]}
-        maxConstraints={[windowWidth, windowHeight * 0.7]}
+        minConstraints={[windowWidth, (windowHeight - 36) * 0.5]}
+        maxConstraints={[windowWidth, windowHeight - 168 - 36]}
         className={cx('upper-section')}
       >
-        <div className={cx('library-panel')}>LP</div>
-        <div className={cx('rendering-panel')}>RP</div>
-        <div className={cx('control-panel')}>CP</div>
+        <Fragment>
+          <div className={cx('library-panel')}>LP</div>
+          <div className={cx('rendering-panel')}>RP</div>
+          <div className={cx('control-panel')}>CP</div>
+        </Fragment>
       </ResizableBox>
       <ResizableBox
         width={windowWidth}
         height={sectionHeight.lowerSection}
-        minConstraints={[windowWidth, windowHeight * 0.3]}
-        maxConstraints={[windowWidth, windowHeight * 0.5]}
+        minConstraints={[windowWidth, 168]}
+        maxConstraints={[windowWidth, (windowHeight) * 0.5]}
         className={cx('lower-section')}
         onResize={handleResize}
-        axis="both"
+        axis="y"
         resizeHandles={['n']}
+
       >
         <div className={cx('timeline-panel')}>TP</div>
-        </ResizableBox>
+      </ResizableBox>
     </div>
   );
 };
