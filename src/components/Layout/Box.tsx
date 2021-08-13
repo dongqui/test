a03cf21f0dcdd0596c8876f5990bd5253e015b05
jjtjs159-jg <61176987+jjtjs159-jg@users.noreply.github.com>
2021-08-14 01:58:29 +0900

    feat: Box 컴포넌트 width, height 옵셔널 대응, id 추가, type 수정

--- src/components/Layout/Box.tsx
+++ src/components/Layout/Box.tsx
@@ -1,28 +1,42 @@
 import { FunctionComponent } from 'react';
 import { ResizableBox, ResizableBoxProps } from 'react-resizable';
+import { useWindowSize } from 'hooks/common';
 import classNames from 'classnames/bind';
 import styles from './Box.module.scss';
 
 const cx = classNames.bind(styles);
 
 interface BaseProps {
+  width?: number;
+  height?: number;
   min?: [number, number];
   max?: [number, number];
 }
 
-type Props = BaseProps & Omit<ResizableBoxProps, 'minConstraints' | 'maxConstraints'>
+type Exclusion = 'width' | 'height' | 'minConstraints' | 'maxConstraints';
+export type BoxProps = BaseProps & Omit<ResizableBoxProps, Exclusion>
+
+type Props = BoxProps & { id: string; }
 
 const Box: FunctionComponent<Props> = ({
+  id,
+  width,
+  height,
   min,
   max,
   className,
   children,
   ...rest
 }) => {
+  const [innerWidth, innerHeight] = useWindowSize();
+
   const classes = cx('wrapper', className);
 
   const params = {
+    id: id,
     className: classes,
+    width: width || innerWidth,
+    height: height || innerHeight,
     minConstraints: min,
     maxConstraints: max,
     ...rest
