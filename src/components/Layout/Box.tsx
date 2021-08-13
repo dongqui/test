50a58c3400884251bfb10600b9751f567f34e01a
jjtjs159-jg <61176987+jjtjs159-jg@users.noreply.github.com>
2021-08-13 13:58:09 +0900

    feat: Bar, Panel을 위한 Box 컴포넌트 정의

---
+++ src/components/Layout/Box.tsx
@@ -0,0 +1,38 @@
+import { FunctionComponent } from 'react';
+import { ResizableBox, ResizableBoxProps } from 'react-resizable';
+import classNames from 'classnames/bind';
+import styles from './Box.module.scss';
+
+const cx = classNames.bind(styles);
+
+interface BaseProps {
+  min?: [number, number];
+  max?: [number, number];
+}
+
+type Props = BaseProps & Omit<ResizableBoxProps, 'minConstraints' | 'maxConstraints'>
+
+const Box: FunctionComponent<Props> = ({
+  min,
+  max,
+  children,
+  ...rest
+}) => {
+  const params = {
+    minConstraints: min,
+    maxConstraints: max,
+    ...rest
+  };
+
+  return (
+    <ResizableBox {...params}>
+      <div className={cx('outer')}>
+        <div className={cx('inner')}>
+          {children}
+        </div>
+      </div>
+    </ResizableBox>
+  );
+};
+
+export default Box;
