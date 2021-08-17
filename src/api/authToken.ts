03e43a8677cd8dd4167ec1d4c1d4e7b3ba7b6f7c
jjtjs159-jg <jjtjs159@gmail.com>
2021-08-17 11:29:45 +0900

    style: TSDoc 주석 타입 삭제

--- src/api/authToken.ts
+++ src/api/authToken.ts
@@ -7,7 +7,7 @@ interface Params {
 /**
  * shoot access를 위한 인증 api
  * 
- * @param {string} token - shoot access를 위한 홈페이지에서 발급받은 인증 토큰
+ * @param token - shoot access를 위한 홈페이지에서 발급받은 인증 토큰
  * @todo 추후, api 안정화 및 팀 프로젝트 개발 여부에 따라 삭제 또는 base url dotenv로 별도 분리 예정
  */
 const authToken = async (params: Params) => {
