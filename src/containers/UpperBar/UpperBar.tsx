2287c25d1aaeb2691cd6d2b9b33088f683f393e1
0xbono <iam@sonjim.in>
2021-08-22 02:32:52 +0900

    feat(containers/UpperBar): UpperBar 기본적인 레이아웃 구성 완료

--- src/containers/UpperBar/UpperBar.tsx
+++ src/containers/UpperBar/UpperBar.tsx
@@ -1,14 +1,46 @@
+import { FilledButton, SegmentButton } from 'components/Button';
+import { IconWrapper, SvgPath } from 'components/Icon';
 import classNames from 'classnames/bind';
 import styles from './UpperBar.module.scss';
 
 const cx = classNames.bind(styles);
 
 const UpperBar: React.FC = () => {
+  const modeList = [
+    {
+      key: 'edit',
+      value: SvgPath.TrackMode,
+      isSelected: true,
+      onClick: () => {},
+    },
+    {
+      key: 'camera',
+      value: SvgPath.Camera,
+      isSelected: false,
+      onClick: () => {},
+    },
+  ];
   return (
     <header className={cx('wrap')}>
-      <div className={cx('left-upper')}></div>
-      <div className={cx('middle-upper')}></div>
-      <div className={cx('right-upper')}></div>
+      <div className={cx('left-upper')}>
+        <div className={cx('void')} />
+        <div className={cx('breadcrumb')}>
+          <span className={cx('breadcrumb-text')}>Project name</span>
+          <span className={cx('breadcrumb-text')}>Scene name</span>
+        </div>
+      </div>
+      <div className={cx('middle-upper')}>
+        <IconWrapper className={cx('reset-icon')} icon={SvgPath.CameraReset} />
+        <SegmentButton list={modeList} />
+      </div>
+      <div className={cx('right-upper')}>
+        <FilledButton className={cx('share-button')} text="Share" />
+        <div className={cx('right-upper-inner')}>
+          {Array.from(Array(2), (_, i) => (
+            <div key={i} className={cx('void')} />
+          ))}
+        </div>
+      </div>
     </header>
   );
 };
