6aba99391ebcb00eadc41f728c673aaada7c2a68
CharmingCheol <toby0806@naver.com>
2021-08-25 17:37:24 +0900

    feat : TopRuler 출력

--- src/containers/Panels/TimelinePanel_New/TimelineEditor/Rulers/TopRuler/index.tsx
+++ src/containers/Panels/TimelinePanel_New/TimelineEditor/Rulers/TopRuler/index.tsx
@@ -1,11 +1,22 @@
-import React from 'react';
+import React, { RefObject } from 'react';
 import classNames from 'classnames/bind';
 import styles from './index.module.scss';
 
 const cx = classNames.bind(styles);
 
-const TopRuler = () => {
-  return <g className={cx('top-ruler')}></g>;
+interface Props {
+  topRulerRef: RefObject<SVGGElement>;
+}
+
+const TopRuler = (props: Props) => {
+  const { topRulerRef } = props;
+
+  return (
+    <g>
+      <rect className={cx('ruler-width')} />
+      <g ref={topRulerRef} className={cx('top-ruler')} />
+    </g>
+  );
 };
 
 export default TopRuler;
