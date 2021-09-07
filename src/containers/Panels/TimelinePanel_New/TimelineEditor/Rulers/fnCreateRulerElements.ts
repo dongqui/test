f0a2d3bd929d51358dde9679dad2b6ff465f63be
CharmingCheol <toby0806@naver.com>
2021-09-07 15:43:59 +0900

    chore: top ruler 생성 함수 이름 변경

--- src/containers/Panels/TimelinePanel_New/TimelineEditor/Rulers/fnCreateRulerElements.ts
+++ src/containers/Panels/TimelinePanel_New/TimelineEditor/Rulers/fnCreateRulerElements.ts
@@ -2,11 +2,11 @@ import * as d3 from 'd3';
 import { D3SVGGElement, D3ScaleLinear } from 'types/TP';
 
 // top ruler의 눈금 숫자 생성
-export const createTopGradation = (topRuler: D3SVGGElement, scaleX: D3ScaleLinear) => {
+export const createTopRulerNumbers = (topRuler: D3SVGGElement, scaleX: D3ScaleLinear) => {
   topRuler.call(d3.axisTop(scaleX));
 };
 
 // top ruler의 grid line 생성
-export const createTopGridLine = (topRuler: D3SVGGElement) => {
+export const createTopRulerGridLine = (topRuler: D3SVGGElement) => {
   topRuler.selectAll('line').attr('y1', -6).attr('y2', 2000);
 };
