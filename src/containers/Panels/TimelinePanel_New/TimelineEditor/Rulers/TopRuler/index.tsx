5d29bcebe0c069a87460483780009471acec1d6d
CharmingCheol <toby0806@naver.com>
2021-09-09 22:55:27 +0900

    fix: 기존 StartEndInput, StartEndRange 컴포넌트 삭제

--- src/containers/Panels/TimelinePanel_New/TimelineEditor/Rulers/TopRuler/index.tsx
+++ src/containers/Panels/TimelinePanel_New/TimelineEditor/Rulers/TopRuler/index.tsx
@@ -1,61 +1,33 @@
-import { useCallback, useState, useRef } from 'react';
-import StartEndRange from './StartEndRange';
+import { useEffect, useRef } from 'react';
+import { useSelector } from 'reducers';
+import ScaleLinear from '../../scaleLinear';
 import classNames from 'classnames/bind';
 import styles from './index.module.scss';
 
 const cx = classNames.bind(styles);
 
 const TopRuler = () => {
-  const [startValue, setStartValue] = useState<number | string>(0); // 공백을 입력하면 ""이 되기 때문에 string type 필요
-  const [endValue, setEndValue] = useState<number | string>(100); // 공백을 입력하면 ""이 되기 때문에 string type 필요
-  const previousStartValue = useRef(0);
-  const previousEndValue = useRef(100);
+  const loopRangeRef = useRef<SVGRectElement>(null);
+  const startTimeIndex = useSelector((state) => state.animatingControls.startTimeIndex);
+  const endTimeIndex = useSelector((state) => state.animatingControls.endTimeIndex);
 
-  // start input blur 이벤트 동작
-  const handleStartInputBlur = useCallback(
-    (event: React.FocusEvent<HTMLInputElement>) => {
-      const nextStartValue = parseInt(event.target.value);
-      const isMinusZero = Object.is(-0, nextStartValue);
-      if (isNaN(nextStartValue) || nextStartValue < 0 || endValue <= nextStartValue) {
-        return setStartValue(previousStartValue.current);
-      }
-      if (isMinusZero) {
-        event.target.value = '0';
-        return setStartValue(0);
-      }
-      setStartValue(nextStartValue);
-      previousStartValue.current = nextStartValue;
-    },
-    [endValue],
-  );
-
-  // end input blur 이벤트 동작
-  const handleEndInputBlur = useCallback(
-    (event: React.FocusEvent<HTMLInputElement>) => {
-      const nextEndValue = parseInt(event.target.value);
-      if (isNaN(nextEndValue) || nextEndValue <= startValue) {
-        return setEndValue(previousEndValue.current);
-      }
-      setEndValue(nextEndValue);
-      previousEndValue.current = nextEndValue;
-    },
-    [startValue],
-  );
-
-  // start input value 업데이트
-  const handleStartInputChange = useCallback((event: React.ChangeEvent<HTMLInputElement>) => {
-    setStartValue(event.target.value);
-  }, []);
-
-  // end input value 업데이트
-  const handleEndInputChange = useCallback((event: React.ChangeEvent<HTMLInputElement>) => {
-    setEndValue(event.target.value);
-  }, []);
+  // start, end 변경 시 range 크기 조절
+  useEffect(() => {
+    const scaleX = ScaleLinear.getScaleX();
+    const loopRange = loopRangeRef.current;
+    if (loopRange && scaleX) {
+      const startTranslateX = scaleX(startTimeIndex);
+      const endTranslateX = scaleX(endTimeIndex);
+      const width = `width:${endTranslateX - startTranslateX}px`;
+      const translate3d = `transform:translate3d(${startTranslateX + 20}px, 0, 0)`;
+      loopRange.style.cssText = `${width}; ${translate3d};`;
+    }
+  }, [endTimeIndex, startTimeIndex]);
 
   return (
     <g>
       <rect className={cx('background')} />
-      <StartEndRange />
+      <rect id="range" className={cx('range')} ref={loopRangeRef} />
       <g id="top-ruler" className={cx('top-ruler')}>
         {/* d3를 통해 눈금, grid line이 들어가는 영역 */}
       </g>
