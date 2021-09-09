f4ecb5ba4b46590e8c52be7bfae277bf6f211f34
0xbono <iam@sonjim.in>
2021-09-09 20:44:45 +0900

    feat(containers): currentMode를 추가하여 trackMode, videoMode 전환 추가

--- src/containers/index.tsx
+++ src/containers/index.tsx
@@ -1,68 +1,71 @@
-import { FunctionComponent, memo, useLayoutEffect, useState } from 'react';
-import { useDispatch } from 'react-redux';
+import { FunctionComponent, memo, useEffect, useLayoutEffect, useState } from 'react';
 import { useRouter } from 'next/router';
 import { authToken } from 'api';
-import Process from 'containers/Process';
-import Extract from 'containers/Extract';
-import Shoot from 'containers/Shoot';
-import { useSelector } from 'reducers';
 import { Capture } from './VideoMode';
+import Shoot from './Shoot';
+// import Process from 'containers/Process';
 
 export type Procedure = 'service' | 'token' | 'success' | 'denied';
 
 const Index: FunctionComponent = () => {
-  const dispatch = useDispatch();
-  const router = useRouter();
+  useEffect(() => {
+    const getShootMode = () => {
+      window.addEventListener('storage', () => {
+        const hasMode = window.localStorage.getItem('shootMode');
+        setCurrentMode(hasMode);
+      });
+    };
+    getShootMode();
+  }, []);
 
+  const router = useRouter();
   const { token } = router.query;
 
-  const [procedure, setProcedure] = useState<Procedure>('service');
-  const [_message, setMessage] = useState('');
+  const [currentMode, setCurrentMode] = useState<string | null>('trackMode');
+  // const [procedure, setProcedure] = useState<Procedure>('service');
 
-  useLayoutEffect(() => {
-    const isTokenLoaded = localStorage.getItem('token');
+  // const [_message, setMessage] = useState('');
 
-    if (isTokenLoaded) {
-      // 로컬스토리지에 토큰이 있는 경우는 이미 인증이 최소 1번 성공한 케이스로 바로 shoot 로드
-      setProcedure('success');
-    }
+  // useLayoutEffect(() => {
+  //   const isTokenLoaded = localStorage.getItem('token');
 
-    if (!isTokenLoaded) {
-      // 토근 인증 절차
-      // 1. API로 쿼리스트링에 있는 토큰을 인증
-      // 단. 그동안 2초는 반드시 delay 시키고 + api 통신 시간 만큼 process container 요청 패턴 로드
-      setTimeout(() => {
-        setProcedure('token');
-      }, 2000);
+  //   if (isTokenLoaded) {
+  //     // 로컬스토리지에 토큰이 있는 경우는 이미 인증이 최소 1번 성공한 케이스로 바로 shoot 로드
+  //     setProcedure('success');
+  //   }
 
-      // 2. 쿼리스트링 토큰 자체가 없는 경우 token 프로세스를 먼저 밟고 denied 처리
-      if (!token) {
-        setTimeout(() => {
-          setProcedure('denied');
-        }, 4000);
-      }
+  //   if (!isTokenLoaded) {
+  //     // 토근 인증 절차
+  //     // 1. API로 쿼리스트링에 있는 토큰을 인증
+  //     // 단. 그동안 2초는 반드시 delay 시키고 + api 통신 시간 만큼 process container 요청 패턴 로드
+  //     setTimeout(() => {
+  //       setProcedure('token');
+  //     }, 2000);
 
-      if (token && typeof token === 'string') {
-        // 3. 강제로 4초 딜레이를 줘서 동일 흐름에서 token 프로세스를 먼저 밟고 이후 api 통신
-        setTimeout(async () => {
-          await authToken({
-            token,
-          })
-            .then(() => {
-              setProcedure('success');
-            })
-            .catch((error) => {
-              setProcedure('denied');
-              setMessage(error.message);
-            });
-        }, 4000);
-      }
-    }
-  }, [token]);
+  //     // 2. 쿼리스트링 토큰 자체가 없는 경우 token 프로세스를 먼저 밟고 denied 처리
+  //     if (!token) {
+  //       setTimeout(() => {
+  //         setProcedure('denied');
+  //       }, 4000);
+  //     }
 
-  const pageInfo = useSelector((state) => state.pageInfo);
-
-  const isShootMode = pageInfo.page === 'shoot';
+  //     if (token && typeof token === 'string') {
+  //       // 3. 강제로 4초 딜레이를 줘서 동일 흐름에서 token 프로세스를 먼저 밟고 이후 api 통신
+  //       setTimeout(async () => {
+  //         await authToken({
+  //           token,
+  //         })
+  //           .then(() => {
+  //             setProcedure('success');
+  //           })
+  //           .catch((error) => {
+  //             setProcedure('denied');
+  //             setMessage(error.message);
+  //           });
+  //       }, 4000);
+  //     }
+  //   }
+  // }, [token]);
 
   // if (procedure !== 'success') {
   //   return (
@@ -72,7 +75,7 @@ const Index: FunctionComponent = () => {
   //   );
   // }
 
-  return <main>{isShootMode ? <Capture /> : <Capture />}</main>;
+  return <main>{currentMode === 'trackMode' ? <Shoot /> : <Capture />}</main>;
 };
 
 export default memo(Index);
