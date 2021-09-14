504f300e1428f54fe67e16c327942c31cb93ddbb
jjtjs159-jg <61176987+jjtjs159-jg@users.noreply.github.com>
2021-09-15 02:04:07 +0900

    feat: LPBody ListNode 임시 추가

--- src/containers/Panels/LibraryPanel/LPBody.tsx
+++ src/containers/Panels/LibraryPanel/LPBody.tsx
@@ -1,13 +1,20 @@
 import { FunctionComponent } from 'react';
+import { ListNode } from './ListView';
 import classNames from 'classnames/bind';
 import styles from './LPBody.module.scss';
 
 const cx = classNames.bind(styles);
 
-interface Props {}
+interface Props {
+  view: LP.View;
+}
 
 const LPBody: FunctionComponent<Props> = () => {
-  return <div className={cx('wrapper')}></div>;
+  return (
+    <div className={cx('wrapper')}>
+      <ListNode type="Folder" />
+    </div>
+  );
 };
 
 export default LPBody;
