46f68c3de6fc48113fabd6996f9b1a83fab99213
jjtjs159-jg <61176987+jjtjs159-jg@users.noreply.github.com>
2021-09-15 01:28:22 +0900

    feat: LP AmbientNamespace 테스트 정의

---
+++ @types/Container/LP.d.ts
@@ -0,0 +1,10 @@
+import { MutableRefObject, InputHTMLAttributes, RefObject } from 'react';
+
+export = LP;
+export as namespace LP;
+
+declare namespace LP {
+  type View = 'List' | 'Gallery';
+}
+
+export default LP;
