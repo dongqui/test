84d8b9dd8e8b535f86fac4c23c31db0a0d3907e2
jjtjs159-jg <jjtjs159@gmail.com>
2021-09-15 19:29:31 +0900

    feat: LP default model load 및 drop에 대한 action, reducer 정의

--- src/actions/LP/lpNodeAction.ts
+++ src/actions/LP/lpNodeAction.ts
@@ -1,16 +1,27 @@
 interface State {
-  node: any;
+  nodes: LP.Node[];
 }
 
-export type LPNodeAction = ReturnType<typeof changeNode>;
+export type LPNodeAction = ReturnType<typeof changeNode> | ReturnType<typeof visualize>;
 
 export const CHANGE_NODE = 'mode/CHANGE_NODE' as const;
+export const VISUALIZE = 'mode/VISUALIZE' as const;
 
-interface changeNode {}
+interface ChangeNodeParams {
+  nodes: LP.Node[];
+}
+interface VisualizeParams {
+  fileURL: string;
+}
 
-export const changeNode = (params: changeNode) => ({
+export const changeNode = (params: ChangeNodeParams) => ({
   type: CHANGE_NODE,
   payload: {
     ...params,
   },
+});
+
+export const visualize = (params: string | File) => ({
+  type: VISUALIZE,
+  payload: params,
 });
