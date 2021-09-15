49ade4c3a6021e17b70ed75556ad040bef9ec76f
jjtjs159-jg <jjtjs159@gmail.com>
2021-09-15 19:28:36 +0900

    feat: LP Node type 정의

--- @types/Container/LP.d.ts
+++ @types/Container/LP.d.ts
@@ -5,6 +5,14 @@ export as namespace LP;
 
 declare namespace LP {
   type View = 'List' | 'Gallery';
+  type NodeType = 'Folder' | 'Model' | 'Motion';
+
+  interface Node {
+    id: string;
+    fileURL: string | File;
+    name: string;
+    type: LP.NodeType;
+  }
 }
 
 export default LP;
