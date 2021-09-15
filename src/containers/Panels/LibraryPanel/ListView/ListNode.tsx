681ad0611b492e3a4182712a3d1d76a286384178
jjtjs159-jg <jjtjs159@gmail.com>
2021-09-15 11:58:09 +0900

    feat: LP IconNode 타입 별 Arrow visibility 설정 및 화살표 클릭 이벤트 임시 정의

--- src/containers/Panels/LibraryPanel/ListView/ListNode.tsx
+++ src/containers/Panels/LibraryPanel/ListView/ListNode.tsx
@@ -1,4 +1,4 @@
-import { FunctionComponent } from 'react';
+import { FunctionComponent, ReactNode, useCallback } from 'react';
 import { IconWrapper, SvgPath } from 'components/Icon';
 import classNames from 'classnames/bind';
 import styles from './ListNode.module.scss';
@@ -7,14 +7,23 @@ const cx = classNames.bind(styles);
 
 interface Props {
   type: 'Folder' | 'Model' | 'Motion';
+  name: ReactNode;
 }
 
-const ListNode: FunctionComponent<Props> = ({ type }) => {
+const ListNode: FunctionComponent<Props> = ({ type, name }) => {
+  const arrowClasses = cx('icon-arrow', {
+    invisible: type === 'Motion',
+  });
+
+  const handleArrowClick = useCallback(() => {
+    console.log('handleArrowClick');
+  }, []);
+
   return (
     <div className={cx('wrapper')}>
-      <IconWrapper icon={SvgPath.FilledArrow} className={cx('icon-arrow')} />
+      <IconWrapper icon={SvgPath.FilledArrow} className={arrowClasses} onClick={handleArrowClick} />
       <IconWrapper icon={SvgPath[type]} className={cx('icon-type')} />
-      <div className={cx('name')}>Directory</div>
+      <div className={cx('name')}>{name}</div>
     </div>
   );
 };
