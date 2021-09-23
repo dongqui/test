5d609a4c0552df1e1e1ea79c4bea45d4970bf576
kennycha <dudqn136@naver.com>
2021-09-23 21:38:51 +0900

    feat: add function for default scene setting
    - camera
    - directional and hemisphere light
    - ground with texture

---
+++ src/utils/RP/createGround.ts
@@ -0,0 +1,47 @@
+import * as BABYLON from '@babylonjs/core';
+
+const defaultWidth = 30;
+const defaultHeight = 30;
+const defaultSubdivision = 30;
+
+/**
+ * ground를 생성합니다.
+ *
+ * @param scene - ground를 생성할 scene
+ * @param useTexture - texture 사용 여부
+ * @param width - ground의 가로 default = 30
+ * @param height - ground의 세로 default = 30
+ * @param subdivision - 한 면당 subdivisions의 수 default = 30
+ */
+const createGround = (
+  scene: BABYLON.Scene,
+  useTexture: boolean,
+  width?: number,
+  height?: number,
+  subdivision?: number,
+) => {
+  const ground = BABYLON.Mesh.CreateGround(
+    'ground',
+    (width = defaultWidth),
+    (height = defaultHeight),
+    (subdivision = defaultSubdivision),
+    scene,
+  );
+  const groundMaterial = new BABYLON.StandardMaterial('groundMaterial', scene);
+
+  if (useTexture) {
+    const diffuseTexture = new BABYLON.Texture('texture/texture_01.png', scene);
+    // texture 반복과 관련한 설정입니다.
+    diffuseTexture.wrapU = BABYLON.Texture.WRAP_ADDRESSMODE;
+    diffuseTexture.wrapV = BABYLON.Texture.WRAP_ADDRESSMODE;
+    diffuseTexture.uScale = 5;
+    diffuseTexture.vScale = 5;
+    groundMaterial.diffuseTexture = diffuseTexture;
+  }
+
+  ground.material = groundMaterial;
+
+  return ground;
+};
+
+export default createGround;
---
+++ src/utils/RP/index.ts
@@ -0,0 +1,4 @@
+export { default as createGround } from './createGround';
+export { default as createCamera } from './createCamera';
+export { default as createHemisphericLight } from './createHemisphericLight';
+export { default as createDirectionalLight } from './createDirectionalLight';
