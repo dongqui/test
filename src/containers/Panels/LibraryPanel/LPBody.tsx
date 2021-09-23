174a463d39b7386381e313222843bbb92e7d2b76
jjtjs159-jg <jjtjs159@gmail.com>
2021-09-23 13:09:45 +0900

    feat: LPBody cursor 위치에 따른 ContextMenu 제어 기능 테스트 코드 추가

--- src/containers/Panels/LibraryPanel/LPBody.tsx
+++ src/containers/Panels/LibraryPanel/LPBody.tsx
@@ -1,4 +1,5 @@
-import { FunctionComponent } from 'react';
+import { FunctionComponent, memo, useEffect, useState, useRef, createRef, RefObject } from 'react';
+import { useContextMenu } from 'new_components/ContextMenu/ContextMenu';
 import { ListNode } from './ListView';
 import classNames from 'classnames/bind';
 import styles from './LPBody.module.scss';
@@ -11,13 +12,95 @@ interface Props {
 }
 
 const LPBody: FunctionComponent<Props> = ({ view, nodes }) => {
+  const { onContextMenuOpen, onContextMenuClose } = useContextMenu();
+
+  const wrapperRef = useRef<HTMLDivElement>(null);
+  // const nodeRef = useRef<HTMLDivElement>(null);
+
+  const [nodeRefs, setNodeRefs] = useState<RefObject<HTMLDivElement>[]>([]);
+
+  useEffect(() => {
+    setNodeRefs(Array.from({ length: nodes.length }).map(() => createRef()));
+  }, [nodes.length]);
+
+  useEffect(() => {
+    const handleContextMenu = (e: MouseEvent) => {
+      e.preventDefault();
+
+      console.log(nodeRefs);
+
+      const isContains = wrapperRef.current?.contains(e.target as Node);
+      const isOutsideNode = !nodeRefs
+        .map((nodeRef, i) => nodeRef.current?.contains(e.target as Node))
+        .some((isNodeContains) => isNodeContains);
+
+      console.log(isOutsideNode);
+
+      if (isContains && isOutsideNode) {
+        console.log('머냐');
+        console.log(e.clientX, e.clientY);
+        onContextMenuOpen({
+          innerRef: wrapperRef,
+          top: e.clientY,
+          left: e.clientX,
+          menu: [
+            {
+              label: '텍스트1',
+              onClick: () => {},
+              children: [
+                {
+                  label: '하위1',
+                  onClick: () => {},
+                },
+                {
+                  label: '하위2',
+                  onClick: () => {},
+                },
+                {
+                  label: '하위3',
+                  onClick: () => {},
+                },
+              ],
+            },
+            {
+              label: '텍스트2',
+              onClick: () => {},
+              children: [],
+            },
+            {
+              label: '텍스트2',
+              onClick: () => {},
+              children: [],
+            },
+          ],
+        });
+      }
+    };
+
+    const currentRef = wrapperRef.current;
+
+    if (currentRef) {
+      currentRef.addEventListener('contextmenu', handleContextMenu);
+
+      return () => {
+        currentRef.removeEventListener('contextmenu', handleContextMenu);
+      };
+    }
+  }, [nodeRefs, onContextMenuOpen]);
+
   return (
-    <div className={cx('wrapper')}>
-      {nodes.map((node) => (
-        <ListNode key={node.id} type={node.type} name={node.name} fileURL={node.fileURL} />
+    <div className={cx('wrapper')} ref={wrapperRef}>
+      {nodes.map((node, i) => (
+        <ListNode
+          ref={nodeRefs[i]}
+          key={node.id}
+          type={node.type}
+          name={node.name}
+          fileURL={node.fileURL}
+        />
       ))}
     </div>
   );
 };
 
-export default LPBody;
+export default memo(LPBody);
