7d20d1d6590856d3f861c027e4b410ac34526b3f
CharmingCheol <toby0806@naver.com>
2021-10-12 19:45:09 +0900

    feat : dopesheetHeader 추가

--- src/containers/MiddleBar/Shoot/MiddleBarHeader/index.tsx
+++ src/containers/MiddleBar/Shoot/MiddleBarHeader/index.tsx
@@ -1,13 +1,15 @@
-import { FunctionComponent } from 'react';
+import DopeSheetHeader from './DopeSheetHeader';
 import classNames from 'classnames/bind';
 import styles from './index.module.scss';
 
 const cx = classNames.bind(styles);
 
-interface Props {}
-
-const MiddleBarHeader: FunctionComponent<Props> = () => {
-  return <div className={cx('header')}></div>;
+const MiddleBarHeader = () => {
+  return (
+    <div className={cx('header')}>
+      <DopeSheetHeader />
+    </div>
+  );
 };
 
 export default MiddleBarHeader;
