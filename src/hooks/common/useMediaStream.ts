11e5d41d714c4ea21a305a83f0039ed6d9e3b309
JinhyukY <markyoo@plask.ai>
2021-10-22 14:56:58 +0900

    feat: VM 관련 기능들을 새로 구현한 useMediaStream 제작

--- src/hooks/common/useMediaStream.ts
+++ src/hooks/common/useMediaStream.ts
@@ -1,89 +1,160 @@
-import { RefObject } from 'react';
+import { RefObject, useCallback, useState, Dispatch, SetStateAction } from 'react';
 
 interface Props {
   ref: RefObject<HTMLVideoElement>;
+  canvasRef: RefObject<HTMLCanvasElement>;
+  setThumbnailList: Dispatch<SetStateAction<never[]>>;
+  setDuration: Dispatch<SetStateAction<number>>;
+  setPlayState: Dispatch<SetStateAction<boolean>>;
+  browserType: string;
 }
 
 const useMediaStream = (props: Props) => {
-  let mediaStreamChunk: BlobPart[] = [];
-  let mediaBlobUrl = null;
+  const { ref, canvasRef, setThumbnailList, setPlayState, setDuration, browserType } = props;
+  const [currentStream, setCurrentStream] = useState<MediaStream>();
+  const [recorderData, setRecorderData] = useState<MediaRecorder>();
 
-  const mediaStream = props.ref.current?.srcObject as MediaStream & HTMLMediaElement;
-
-  /**
-   * @Description mediaStream 활성화
-   */
-  const mediaStreamInitialize = () => {
+  const mediaStreamInitialize = useCallback(() => {
     navigator.mediaDevices.getUserMedia({ video: true }).then((stream) => {
-      props.ref.current!.srcObject = stream;
+      ref.current!.srcObject = stream;
+      setCurrentStream(stream);
     });
-  };
+  }, [ref]);
 
-  /**
-   * @Description 사용 가능한 기기를 리턴합니다.
-   * @returns MediaDeviceInfo[]
-   */
-  const availableDevices = () => {
-    navigator.mediaDevices.enumerateDevices().then((devices) => {
-      const result = devices.filter((device) => device.kind === 'videoinput');
-      return result;
-    });
-  };
+  const stopStream = useCallback(() => {
+    if (currentStream && ref.current!.srcObject) {
+      const tracks = currentStream.getTracks();
+      // const stream = ref.current!.srcObject as MediaStream;
+      // const tracks2 = stream.getTracks();
 
-  /**
-   * @Description MediaRecorder 시작
-   */
-  const startRecording = () => {
-    recorder.start();
-  };
-
-  /**
-   * @Description MediaRecorder 중단
-   */
-  const stopRecording = () => {
-    recorder.stop();
-    mediaStream.srcObject = null;
-  };
-
-  /**
-   * @Description MediaRecorder 중지
-   */
-  const pauseRecording = () => {
-    recorder.pause();
-  };
-
-  /**
-   * @Description MimeType 선언 및 medaiStream 데이터 대입
-   * !@see TypeError
-   */
-  const recorder = new MediaRecorder(mediaStream, {
-    mimeType: 'video/mp4',
-  });
-
-  /**
-   * @Description 녹화 중에 BlobEvent가 전달되었을 때 이벤트
-   */
-  recorder.ondataavailable = (event: any) => {
-    if (event.data && event.data.size > 0) {
-      mediaStreamChunk.push(event.data);
+      tracks.forEach((track) => track.stop());
+      // tracks2.forEach((track: any) => track.stop());
+      ref.current!.srcObject = null;
     }
-  };
+  }, [currentStream, ref]);
 
-  /**
-   * @Description 녹화가 종료되었을 때 이벤트
-   */
-  recorder.onstop = () => {
-    const blob = new Blob(mediaStreamChunk, { type: 'video/webm' });
-    mediaBlobUrl = URL.createObjectURL(blob);
-  };
+  const availableDevices = useCallback(() => {}, []);
+
+  const handleScreenshot = useCallback(() => {
+    if (canvasRef && ref) {
+      canvasRef.current!.width = ref.current!.videoWidth;
+      canvasRef.current!.height = ref.current!.videoHeight;
+
+      const canvasContext = canvasRef.current!.getContext('2d');
+      canvasContext!.drawImage(ref.current as CanvasImageSource, 0, 0);
+
+      return canvasRef.current?.toDataURL('image/webp');
+    }
+  }, [canvasRef, ref]);
+
+  const handleMetaData = useCallback(() => {
+    let count = 0;
+    let thumbnailList: any = [];
+    let dividedDuration: number = 0;
+
+    const checkDuration = setInterval(() => {
+      if (ref.current!.duration !== Infinity) {
+        console.log('here to check if this setINterval is activated more than once');
+        ref.current!.pause();
+        ref.current!.currentTime = 0;
+        clearInterval(checkDuration);
+        dividedDuration = ref.current!.duration / 20;
+        setDuration(ref.current!.duration);
+
+        const setScreenshot = setInterval(() => {
+          if (count < 20) {
+            console.log('duration: ', dividedDuration);
+            count++;
+            thumbnailList = [...thumbnailList, handleScreenshot()];
+            ref.current!.currentTime += dividedDuration;
+          } else {
+            ref.current!.currentTime = 0;
+            setThumbnailList(thumbnailList);
+            clearInterval(setScreenshot);
+          }
+        }, 150);
+      }
+    }, 500);
+  }, [ref, handleScreenshot, setThumbnailList, setDuration]);
+
+  const startRecording = useCallback(() => {
+    if (recorderData && recorderData.state === 'recording') {
+      console.log('already recording');
+      return;
+    }
+    if (currentStream) {
+      setThumbnailList([]);
+      let recorder = new MediaRecorder(currentStream, {
+        mimeType: browserType === 'safari' ? 'video/mp4' : 'video/webm',
+      });
+      let blobs: Blob[] = [];
+
+      if (recorder.state === 'inactive') {
+        mediaStreamInitialize();
+      }
+
+      recorder.ondataavailable = (e) => {
+        console.log('data pushed', e);
+        blobs.push(e.data);
+      };
+
+      recorder.onstop = () => {
+        let video_local = URL.createObjectURL(
+          new Blob(blobs, { type: browserType === 'safari' ? 'video/mp4' : 'video/webm' }),
+        );
+        stopStream();
+        ref.current!.src = video_local;
+      };
+
+      recorder.start(1000);
+
+      setRecorderData(recorder);
+    }
+  }, [
+    recorderData,
+    currentStream,
+    mediaStreamInitialize,
+    browserType,
+    ref,
+    stopStream,
+    setThumbnailList,
+  ]);
+
+  const stopRecording = useCallback(
+    async (e) => {
+      if (recorderData && recorderData.onstop) {
+        if (recorderData.state === 'recording') {
+          recorderData.onstop(e);
+          handleMetaData();
+          recorderData.stop();
+        }
+      }
+    },
+    [recorderData, handleMetaData],
+  );
+
+  const playRecording = useCallback(() => {
+    if (ref) {
+      setPlayState(true);
+      ref.current!.play();
+    }
+  }, [ref, setPlayState]);
+
+  const pauseRecording = useCallback(() => {
+    if (ref) {
+      setPlayState(false);
+      ref.current!.pause();
+    }
+  }, [ref, setPlayState]);
 
   return {
     mediaStreamInitialize,
     availableDevices,
     startRecording,
     stopRecording,
+    playRecording,
     pauseRecording,
-    mediaBlobUrl,
+    // mediaBlobUrl,
   };
 };
 
