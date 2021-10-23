ef75e84e8a8d230ca5d3278270c5af27108a55d8
CharmingCheol <toby0806@naver.com>
2021-10-23 23:30:28 +0900

    feat : TimeIndex value 업데이트

--- src/reducers/animatingControls.ts
+++ src/reducers/animatingControls.ts
@@ -1,4 +1,5 @@
 import { AnimatingControlsAction } from 'actions/animatingControls';
+import { TimeIndex } from 'utils/TP';
 import { PlayDirection_New, PlayState } from 'types/RP';
 
 interface AnimatingContolsState {
@@ -22,23 +23,31 @@ const defaultState: AnimatingContolsState = {
 export const animatingControls = (state = defaultState, action: AnimatingControlsAction) => {
   switch (action.type) {
     case 'animatingControls/BLUR_START_INPUT': {
+      TimeIndex.setStartTimeIndex(action.payload.startTimeIndex);
+      TimeIndex.setCurrentTimeIndex(action.payload.currentTimeIndex);
       return Object.assign({}, state, {
         startTimeIndex: action.payload.startTimeIndex,
         currentTimeIndex: action.payload.currentTimeIndex,
       });
     }
     case 'animatingControls/BLUR_END_INPUT': {
+      TimeIndex.setEndTimeIndex(action.payload.endTimeIndex);
+      TimeIndex.setCurrentTimeIndex(action.payload.currentTimeIndex);
       return Object.assign({}, state, {
         endTimeIndex: action.payload.endTimeIndex,
         currentTimeIndex: action.payload.currentTimeIndex,
       });
     }
     case 'animatingControls/MOVE_SCRUBBER': {
+      TimeIndex.setCurrentTimeIndex(action.payload.currentTimeIndex);
       return Object.assign({}, state, {
         currentTimeIndex: action.payload.currentTimeIndex,
       });
     }
     case 'animatingControls/CLICK_PLAY_STATE_BUTTON': {
+      if (typeof action.payload.currentTimeIndex === 'number') {
+        TimeIndex.setCurrentTimeIndex(action.payload.currentTimeIndex);
+      }
       return Object.assign({}, state, action.payload);
     }
     case 'animatingControls/SELECT_FASTER_DROPDOWN': {
