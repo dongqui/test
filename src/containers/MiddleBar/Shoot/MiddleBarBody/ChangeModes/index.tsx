ad3745cba4b2f916733c246f86049df704a9592b
CharmingCheol <toby0806@naver.com>
2021-10-23 23:24:23 +0900

    feat : InsertKeyframe, AutoKey 컴포넌트 분리

--- src/containers/MiddleBar/Shoot/MiddleBarBody/ChangeModes/index.tsx
+++ src/containers/MiddleBar/Shoot/MiddleBarBody/ChangeModes/index.tsx
@@ -1,7 +1,11 @@
 import { TextButton } from 'components/Button';
 import { IconWrapper, SvgPath } from 'components/Icon';
+
 import InterpolationMode from './InterpolationMode';
+import InsertKeyframe from './InsertKeyframe';
+import AutoKey from './AutoKey';
 import SimpleMode from './SimpleMode';
+
 import classNames from 'classnames/bind';
 import styles from './index.module.scss';
 
@@ -12,8 +16,8 @@ const ChangeModes = () => {
     <div className={cx('change-modes')}>
       <InterpolationMode />
       <SimpleMode />
-      <IconWrapper icon={SvgPath.InsertKeyframe} hasFrame={false} />
-      <TextButton text="Autokey" />
+      <InsertKeyframe />
+      <AutoKey />
     </div>
   );
 };
