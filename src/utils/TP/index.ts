97b46458260020862197bf8271c9c2d791cef4dd
CharmingCheol <toby0806@naver.com>
2021-10-25 18:26:41 +0900

    feat : getTransformXIndex 추가

--- src/utils/TP/index.ts
+++ src/utils/TP/index.ts
@@ -1,4 +1,5 @@
 export { default as getBinarySearch } from './getBinarySearch';
 export { default as getBoneTrackIndex } from './getBoneTrackIndex';
+export { default as getTransformXIndex } from './getTransformXIndex';
 export { default as ScaleLinear } from './ScaleLinear';
 export { default as TimeIndex } from './TimeIndex';
