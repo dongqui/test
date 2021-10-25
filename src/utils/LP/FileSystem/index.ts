85d8aebbb334406e23dc8ff28fe6a47d0148fe6a
jjtjs159-jg <61176987+jjtjs159-jg@users.noreply.github.com>
2021-10-26 01:09:45 +0900

    feat(LP/rename): LP Rename 전처리 함수 임시 정의

--- src/utils/LP/FileSystem/index.ts
+++ src/utils/LP/FileSystem/index.ts
@@ -1,3 +1,4 @@
 export { default as beforePaste } from './beforePaste';
+export { default as beforeRename } from './beforeRename';
 export { default as getNodeNumber } from './getNodeNumber';
 export { default as duplicateCheck } from './duplicateCheck';
