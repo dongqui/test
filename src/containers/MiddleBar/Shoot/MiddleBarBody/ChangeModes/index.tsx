727e20fe6d35732645d7a7af46e0ea6f2f2adbca
CharmingCheol <toby0806@naver.com>
2021-10-26 09:00:55 +0900

    refactor : interpoltaion mode 버튼 화면에서 비활성화

--- src/containers/MiddleBar/Shoot/MiddleBarBody/ChangeModes/index.tsx
+++ src/containers/MiddleBar/Shoot/MiddleBarBody/ChangeModes/index.tsx
@@ -1,7 +1,3 @@
-import { TextButton } from 'components/Button';
-import { IconWrapper, SvgPath } from 'components/Icon';
-
-import InterpolationMode from './InterpolationMode';
 import InsertKeyframe from './InsertKeyframe';
 import AutoKey from './AutoKey';
 import SimpleMode from './SimpleMode';
@@ -14,7 +10,6 @@ const cx = classNames.bind(styles);
 const ChangeModes = () => {
   return (
     <div className={cx('change-modes')}>
-      <InterpolationMode />
       <SimpleMode />
       <InsertKeyframe />
       <AutoKey />
