2725299b3e5bf67cd33692faea52a97190e72be6
jjtjs159-jg <jjtjs159@gmail.com>
2021-10-26 16:27:46 +0900

    feat(LP/rename): Rename 유틸 함수 bugfix 및 error throw

--- src/utils/LP/FileSystem/beforeRename.ts
+++ src/utils/LP/FileSystem/beforeRename.ts
@@ -1,46 +1,29 @@
-import _ from 'lodash';
-import duplicateCheck from './duplicateCheck';
-
 interface Params {
   name: string;
-  nameArray: string[];
+  comparison: string[];
 }
 
-const beforeRename = (params: Params) => {
-  const { name, nameArray } = params;
+const beforeRename = async (params: Params) => {
+  const { name, comparison } = params;
 
-  const isAlreadyCopy = !_.isNull(name.match(/copy/g));
+  const getResultName = async () => {
+    const tempName = name.trim();
+    const isAlreadyExist = comparison.some((nodeName) => nodeName === tempName);
 
-  let check = '0';
-  let result = '';
-
-  if (isAlreadyCopy) {
-    const matchNumber = name.match(/\(/g);
-    const hasNumber = !_.isNull(matchNumber);
-
-    if (matchNumber !== null) {
-      if (matchNumber.length === 1) {
-        check = duplicateCheck(name, nameArray);
-
-        result = check === '0' ? `${name} (2)` : `${name} (${check})`;
-      }
-
-      if (matchNumber.length > 1) {
-        const tempResult = name.substr(0, name.lastIndexOf('(')).trim();
-
-        check = duplicateCheck(tempResult, nameArray);
-
-        result = check === '0' ? tempResult : `${tempResult} (${check})`;
-      }
+    if (isAlreadyExist) {
+      throw new Error(`${name} 이름이 이미 사용 중입니다. 다른 이름을 선택하십시오.`);
     }
-  }
 
-  if (!isAlreadyCopy) {
-    check = duplicateCheck(name, nameArray);
-    result = check === '0' ? name : `${name} (${check})`;
-  }
+    return tempName;
+  };
 
-  return result;
+  try {
+    const resultName = getResultName();
+
+    return resultName;
+  } catch (error) {
+    throw error;
+  }
 };
 
 export default beforeRename;
