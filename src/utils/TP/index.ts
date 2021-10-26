fa21af8870b6b7b654df75b49d9c50a6ab8d3f38
CharmingCheol <toby0806@naver.com>
2021-10-26 09:50:39 +0900

    remove : getTransformXIndex 함수 삭제

--- src/utils/TP/index.ts
+++ src/utils/TP/index.ts
@@ -1,5 +1,4 @@
 export { default as getBinarySearch } from './getBinarySearch';
 export { default as getBoneTrackIndex } from './getBoneTrackIndex';
-export { default as getTransformXIndex } from './getTransformXIndex';
 export { default as ScaleLinear } from './ScaleLinear';
 export { default as TimeIndex } from './TimeIndex';
