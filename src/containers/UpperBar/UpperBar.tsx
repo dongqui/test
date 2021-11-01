af62631f78e7c9f2709f3784d743f5aebfd48d55
JinhyukY <markyoo@plask.ai>
2021-11-01 11:51:15 +0900

    feat: 카메라 선택 로직 추가 (디자인 미구현)

--- src/containers/UpperBar/UpperBar.tsx
+++ src/containers/UpperBar/UpperBar.tsx
@@ -3,14 +3,25 @@ import { FilledButton, SegmentButton } from 'components/Button';
 import { IconWrapper, SvgPath } from 'components/Icon';
 import classNames from 'classnames/bind';
 import styles from './UpperBar.module.scss';
+import { RefObject } from 'hoist-non-react-statics/node_modules/@types/react';
 
 const cx = classNames.bind(styles);
 
 interface Props {
   sceneName: string;
+  cameraListRef?: RefObject<HTMLDivElement>;
+  deviceList?: MediaDeviceInfo[];
+  currentDevice?: string;
+  handleChangeCamera?: (e: any) => void;
 }
 
-const UpperBar: FunctionComponent<Props> = ({ sceneName }) => {
+const UpperBar: FunctionComponent<Props> = ({
+  sceneName,
+  cameraListRef,
+  currentDevice,
+  handleChangeCamera,
+  deviceList,
+}) => {
   const modeList = [
     {
       key: 'trackMode',
@@ -25,6 +36,7 @@ const UpperBar: FunctionComponent<Props> = ({ sceneName }) => {
       onClick: () => {},
     },
   ];
+
   return (
     <div className={cx('wrap')}>
       <div className={cx('left-upper')}>
@@ -39,11 +51,24 @@ const UpperBar: FunctionComponent<Props> = ({ sceneName }) => {
       </div>
       <div className={cx('right-upper')}>
         <FilledButton className={cx('share-button')} text="Share" />
-        <div className={cx('right-upper-inner')}>
-          {Array.from(Array(2), (_, i) => (
+        <div className={cx('device-select')}>{deviceList && deviceList[0]?.label}</div>
+        <ul className={cx('right-upper-inner')}>
+          {/* {Array.from(Array(2), (_, i) => (
             <div key={i} className={cx('void')} />
-          ))}
-        </div>
+          ))} */}
+          {deviceList &&
+            deviceList.map((device, idx) => (
+              <li
+                key={idx}
+                id={device.deviceId}
+                className={cx('device-select-dropdown')}
+                onClick={handleChangeCamera}
+                data-value
+              >
+                {device.label}
+              </li>
+            ))}
+        </ul>
       </div>
     </div>
   );
