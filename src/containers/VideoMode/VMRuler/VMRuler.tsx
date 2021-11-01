7a630615dfdd0042fc5d13b4e46a682a97413cea
JinhyukY <markyoo@plask.ai>
2021-11-01 11:49:25 +0900

    feat: VM ruler 수치 계산 추가

--- src/containers/VideoMode/VMRuler/VMRuler.tsx
+++ src/containers/VideoMode/VMRuler/VMRuler.tsx
@@ -12,19 +12,22 @@ interface Props {
 
 export const VMRuler: FunctionComponent<Props> = ({ start, end }) => {
   let rulerTime: number[] = [];
+  let roundEnd = Math.round((end + Number.EPSILON) * 10) / 10;
+
   const loopFrame = (lastTime: number) => {
     for (let index = 1; index <= 9; index++) {
-      rulerTime.push(Math.floor(lastTime * Number(`0.${index}`)));
+      const rulerNum = Math.round(lastTime * index) / 10;
+      rulerTime.push(rulerNum);
     }
   };
-  loopFrame(end);
+  loopFrame(roundEnd);
   return (
     <div className={cx('ruler')}>
       <span>{start}</span>
       {rulerTime.map((item, i) => (
         <span key={i}>{item}</span>
       ))}
-      <span>{end}</span>
+      <span>{roundEnd}</span>
     </div>
   );
 };
