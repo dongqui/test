1aa88960b47d871a5a27fd0d1a8cd4bd42167420
kennycha <dudqn136@naver.com>
2021-11-19 17:28:43 +0900

    refactor: redux state 네이밍 규칙 적용

--- src/hooks/RP/useAnimation.ts
+++ src/hooks/RP/useAnimation.ts
@@ -1,23 +1,23 @@
 import { useEffect, useState } from 'react';
-import * as BABYLON from '@babylonjs/core';
 import { useSelector } from 'reducers';
-import { filterQuaternion, filterVector } from 'utils/RP';
+import * as BABYLON from '@babylonjs/core';
 import { round } from 'lodash';
+import { filterQuaternion, filterVector } from 'utils/RP';
 
 const useAnimation = () => {
-  const screenList = useSelector((state) => state.plaskProject.screenList);
-  const assetList = useSelector((state) => state.plaskProject.assetList);
-  const visualizedAssetIds = useSelector((state) => state.plaskProject.visualizedAssetIds);
-  const fps = useSelector((state) => state.plaskProject.fps);
+  const _screenList = useSelector((state) => state.plaskProject.screenList);
+  const _assetList = useSelector((state) => state.plaskProject.assetList);
+  const _visualizedAssetIds = useSelector((state) => state.plaskProject.visualizedAssetIds);
+  const _fps = useSelector((state) => state.plaskProject.fps);
 
-  const animationIngredients = useSelector((state) => state.animationData.animationIngredients);
+  const _animationIngredients = useSelector((state) => state.animationData.animationIngredients);
 
-  const playState = useSelector((state) => state.animatingControls.playState);
-  const playDirection = useSelector((state) => state.animatingControls.playDirection);
-  const playSpeed = useSelector((state) => state.animatingControls.playSpeed);
-  const startTimeIndex = useSelector((state) => state.animatingControls.startTimeIndex);
-  const endTimeIndex = useSelector((state) => state.animatingControls.endTimeIndex);
-  const currentTimeIndex = useSelector((state) => state.animatingControls.currentTimeIndex);
+  const _playState = useSelector((state) => state.animatingControls.playState);
+  const _playDirection = useSelector((state) => state.animatingControls.playDirection);
+  const _playSpeed = useSelector((state) => state.animatingControls.playSpeed);
+  const _startTimeIndex = useSelector((state) => state.animatingControls.startTimeIndex);
+  const _endTimeIndex = useSelector((state) => state.animatingControls.endTimeIndex);
+  const _currentTimeIndex = useSelector((state) => state.animatingControls.currentTimeIndex);
 
   const [currentAnimationGroup, setCurrentAnimationGroup] = useState<BABYLON.AnimationGroup>();
 
@@ -25,19 +25,19 @@ const useAnimation = () => {
   //   console.log('animationIngredients: ', animationIngredients);
   // }, [animationIngredients]);
 
-  // useEffect(() => {
-  //   console.log('fps: ', fps);
-  //   console.log('playState: ', playState);
-  //   console.log('playDirection: ', playDirection);
-  //   console.log('playSpeed: ', playSpeed);
-  //   console.log('startTimeIndex: ', startTimeIndex);
-  //   console.log('endTimeIndex: ', endTimeIndex);
-  // }, [endTimeIndex, fps, playDirection, playSpeed, playState, startTimeIndex]);
+  useEffect(() => {
+    console.log('fps: ', _fps);
+    console.log('playState: ', _playState);
+    console.log('playDirection: ', _playDirection);
+    console.log('playSpeed: ', _playSpeed);
+    console.log('startTimeIndex: ', _startTimeIndex);
+    console.log('endTimeIndex: ', _endTimeIndex);
+  }, [_endTimeIndex, _fps, _playDirection, _playSpeed, _playState, _startTimeIndex]);
 
   // 애니메이션 생성
   useEffect(() => {
-    const visualizedAnimationIngredients = animationIngredients.filter(
-      (animationIngredient) => visualizedAssetIds.includes(animationIngredient.assetId) && animationIngredient.current,
+    const visualizedAnimationIngredients = _animationIngredients.filter(
+      (animationIngredient) => _visualizedAssetIds.includes(animationIngredient.assetId) && animationIngredient.current,
     );
 
     const newAnimationGroup = new BABYLON.AnimationGroup('totalAnimationGroup');
@@ -51,58 +51,57 @@ const useAnimation = () => {
           // rotation track은 단순히 TP내 렌더링 역할만을 하며, 애니메이션 생성 시에는 rotationQuaternion track을 사용
           if (track.isIncluded) {
             if (track.property === 'position' || track.property === 'scaling') {
-              const newAnimation = new BABYLON.Animation(track.name, `${track.property}`, fps, BABYLON.Animation.ANIMATIONTYPE_VECTOR3, BABYLON.Animation.ANIMATIONLOOPMODE_CYCLE);
+              const newAnimation = new BABYLON.Animation(track.name, `${track.property}`, _fps, BABYLON.Animation.ANIMATIONTYPE_VECTOR3, BABYLON.Animation.ANIMATIONLOOPMODE_CYCLE);
               if (track.useFilter) {
                 // filter function 적용
                 newAnimation.setKeys(
-                  filterVector(track.transformKeys, track.filterMinCutoff, track.filterBeta).map((key) => ({ frame: round(key.frame * fps), value: key.value })),
+                  filterVector(track.transformKeys, track.filterMinCutoff, track.filterBeta).map((key) => ({ frame: round(key.frame * _fps), value: key.value })),
                 );
               } else {
-                newAnimation.setKeys(track.transformKeys.map((key) => ({ frame: round(key.frame * fps), value: key.value })));
+                newAnimation.setKeys(track.transformKeys.map((key) => ({ frame: round(key.frame * _fps), value: key.value })));
               }
               track.target.animations.push(newAnimation);
-              newAnimationGroup.addTargetedAnimation(newAnimation, track.target);
+              newAnimationGroup.addTargetedAnimation(newAnimation.clone(), track.target);
             } else if (track.property === 'rotationQuaternion') {
               const newAnimation = new BABYLON.Animation(
                 track.name,
                 `${track.property}`,
-                fps,
+                _fps,
                 BABYLON.Animation.ANIMATIONTYPE_QUATERNION,
                 BABYLON.Animation.ANIMATIONLOOPMODE_CYCLE,
               );
               if (track.useFilter) {
                 // filter function 적용
                 newAnimation.setKeys(
-                  filterQuaternion(track.transformKeys, track.filterMinCutoff, track.filterBeta).map((key) => ({ frame: round(key.frame * fps), value: key.value })),
+                  filterQuaternion(track.transformKeys, track.filterMinCutoff, track.filterBeta).map((key) => ({ frame: round(key.frame * _fps), value: key.value })),
                 );
               } else {
-                newAnimation.setKeys(track.transformKeys.map((key) => ({ frame: round(key.frame * fps), value: key.value })));
+                newAnimation.setKeys(track.transformKeys.map((key) => ({ frame: round(key.frame * _fps), value: key.value })));
               }
               track.target.animations.push(newAnimation);
-              newAnimationGroup.addTargetedAnimation(newAnimation, track.target);
+              newAnimationGroup.addTargetedAnimation(newAnimation.clone(), track.target);
             }
           }
         }
       });
     });
-
+    // newAnimationGroup.normalize(_startTimeIndex, _endTimeIndex);
     setCurrentAnimationGroup(newAnimationGroup);
-  }, [animationIngredients, fps, visualizedAssetIds]);
+  }, [_animationIngredients, _endTimeIndex, _fps, _startTimeIndex, _visualizedAssetIds]);
 
   // 애니메이션 재생 조작
   useEffect(() => {
-    screenList.forEach((PlaskScreen) => {
+    _screenList.forEach((PlaskScreen) => {
       const { id: sceneId, scene, canvasId } = PlaskScreen;
 
       if (currentAnimationGroup) {
-        // scene.addAnimationGroup(currentAnimationGroup);
-
-        switch (playState) {
+        switch (_playState) {
           case 'play': {
             if (currentAnimationGroup.isStarted) {
+              currentAnimationGroup.speedRatio = _playSpeed * _playDirection;
               currentAnimationGroup.play(true);
             } else {
-              currentAnimationGroup.start(true, playSpeed * playDirection, startTimeIndex, endTimeIndex);
+              currentAnimationGroup.start(true, _playSpeed * _playDirection, _startTimeIndex, _endTimeIndex);
             }
             break;
           }
@@ -112,7 +111,7 @@ const useAnimation = () => {
           }
           case 'stop': {
             currentAnimationGroup.pause();
-            currentAnimationGroup.goToFrame(startTimeIndex);
+            currentAnimationGroup.goToFrame(_startTimeIndex);
             break;
           }
           default: {
@@ -121,7 +120,7 @@ const useAnimation = () => {
         }
       }
     });
-  }, [currentAnimationGroup, endTimeIndex, playDirection, playSpeed, playState, screenList, startTimeIndex]);
+  }, [currentAnimationGroup, _endTimeIndex, _playDirection, _playSpeed, _playState, _screenList, _startTimeIndex]);
 };
 
 export default useAnimation;
