927d09d0847591bd84a94e19f63c80753e604d92
JinhyukY <markyoo@plask.ai>
2021-11-22 16:58:39 +0900

    feat: dropdown 버튼과 해당 dropdown의 title을 함께 가지는 Wrapper

---
+++ src/components/ControlPanel/DropdownWrapper.tsx
@@ -0,0 +1,29 @@
+import { FunctionComponent, useState, Dispatch } from 'react';
+import { SetStateAction } from 'hoist-non-react-statics/node_modules/@types/react';
+import { Dropdown } from 'components/ControlPanel';
+import classNames from 'classnames/bind';
+import styles from './DropdownWrapper.module.scss';
+
+interface Props {
+  className?: string;
+  text: string;
+  currentOption: string;
+  setCurrentOption: Dispatch<SetStateAction<string>>;
+  options: string[];
+  activeStatus?: boolean;
+}
+
+const cx = classNames.bind(styles);
+
+const DropdownWrapper: FunctionComponent<Props> = ({ className, text, options, activeStatus, currentOption, setCurrentOption }) => {
+  const classes = cx('wrapper', className, { able: activeStatus === undefined ? true : activeStatus });
+
+  return (
+    <div className={cx(classes)}>
+      <p>{text}</p>
+      <Dropdown options={options} currentOption={currentOption} setCurrentOption={setCurrentOption} />
+    </div>
+  );
+};
+
+export default DropdownWrapper;
