e2938c3a1d83ede94accc848bcdb05441847e445
JinhyukY <markyoo@plask.ai>
2021-11-23 16:31:17 +0900

    feat: Retargeting Panel에서 사용하는 Dropdown 공용 컴포넌트

--- src/components/ControlPanel/Dropdown.tsx
+++ src/components/ControlPanel/Dropdown.tsx
@@ -5,19 +5,23 @@ import classNames from 'classnames/bind';
 import styles from './Dropdown.module.scss';
 
 interface Props {
+  className?: string;
   options: string[];
   currentOption: string;
   setCurrentOption: Dispatch<SetStateAction<string>>;
+  activeStatus?: boolean;
 }
 
 const cx = classNames.bind(styles);
 
-const Dropdown: FunctionComponent<Props> = ({ options, currentOption, setCurrentOption }) => {
+const Dropdown: FunctionComponent<Props> = ({ className, options, currentOption, setCurrentOption, activeStatus }) => {
   const [activeDropdown, setActiveDropdown] = useState<boolean>(false);
+
+  const classes = cx('dropdown-wrapper', className, { able: activeStatus === undefined ? true : activeStatus });
 
   return (
     <Fragment>
-      <div className={cx('dropdown-wrapper')}>
+      <div className={cx(classes)}>
         <div className={cx('dropdown-button')} onClick={() => setActiveDropdown(!activeDropdown)}>
           <div className={cx('dropdown-text')}>{currentOption}</div>
           <IconWrapper className={cx('arrow-down-icon')} icon={SvgPath.EmptyDownArrow} />
