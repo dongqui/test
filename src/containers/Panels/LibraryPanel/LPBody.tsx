948c9522713792a4acfe139a1eaa1ae0ffcc1f49
jjtjs159-jg <jjtjs159@gmail.com>
2021-11-26 14:27:19 +0900

    chore: 잘못된 함수 파라미터 삭제

--- src/containers/Panels/LibraryPanel/LPBody.tsx
+++ src/containers/Panels/LibraryPanel/LPBody.tsx
@@ -169,7 +169,7 @@ const LPBody: FunctionComponent<Props> = ({ lpNode, isPreventContextmenu }) => {
   const [selectedId, setSelectedId] = useState<string[]>([]);
 
   const handleSelect = useCallback(
-    (id: string, childrens?: string[], multiple?: boolean) => {
+    (id: string, multiple?: boolean) => {
       if (multiple) {
         const nextSelectedIds = produce(selectedId, (draft) => {
           if (!draft.includes(id)) {
