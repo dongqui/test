4d459fd6ef2157e69c46e8b8077aba3839cc7d68
jjtjs159-jg <61176987+jjtjs159-jg@users.noreply.github.com>
2021-12-02 17:21:59 +0900

    feat(constants/text): 텍스트 상수 정의

---
+++ src/constants/Text.ts
@@ -0,0 +1,5 @@
+export const WARNING_02 = 'Upload one file at a time.';
+export const WARNING_03 = 'Unsupported file format.';
+export const WARNING_07 = 'File import failed: Should the problem recur, message contact@plask.ai.';
+
+export const CONFIRM_01 = 'Export motion from the video?';
