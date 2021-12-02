5f75b00d838030780a3a9971666315ba77f7394f
JinhyukY <markyoo@plask.ai>
2021-12-02 12:52:09 +0900

    feat: Retargeting Panel 적용 Mapped 상태를 표시하는 indicator 추가

---
+++ src/components/ControlPanel/RetargetMapIndicator.tsx
@@ -0,0 +1,23 @@
+import { FunctionComponent } from 'react';
+import classnames from 'classnames/bind';
+import styles from './RetargetMapIndicator.module.scss';
+import { IconWrapper, SvgPath } from 'components/Icon';
+
+const cx = classnames.bind(styles);
+
+interface Props {
+  isMapped: boolean;
+}
+
+const RetargetMapIndicator: FunctionComponent<Props> = ({ isMapped }) => {
+  const classes = cx('map-indicator', { mapped: isMapped });
+
+  return (
+    <div className={cx(classes)}>
+      <IconWrapper className={cx('indicate-icon')} icon={isMapped ? SvgPath.CheckThin : SvgPath.Warning} />
+      Mapped
+    </div>
+  );
+};
+
+export default RetargetMapIndicator;
--- src/components/ControlPanel/index.tsx
+++ src/components/ControlPanel/index.tsx
@@ -4,3 +4,4 @@ export { default as AnimationTitleToggle } from './AnimationTitleToggle';
 export { default as AnimationRangeInput } from './AnimationRangeInput';
 export { default as Dropdown } from './Dropdown';
 export { default as DropdownWrapper } from './DropdownWrapper';
+export { default as RetargetMapIndicator } from './RetargetMapIndicator';
