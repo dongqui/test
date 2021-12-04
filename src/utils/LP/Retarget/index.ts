a508ea9bd9bc0b55befeff396011e380987af339
kennycha <dudqn136@naver.com>
2021-12-04 18:11:52 +0900

    feat: mocapData 타입 정의

--- src/utils/LP/Retarget/index.ts
+++ src/utils/LP/Retarget/index.ts
@@ -1,5 +1,3 @@
 export { default as createAutoRetargetMap } from './createAutoRetargetMap';
 export { default as createEmptyRetargetMap } from './createEmptyRetargetMap';
-
-// dummy
-// export { default as autoRetarget } from './autoRetarget';
+export { default as retargetMocapData } from './retargetMocapData';
---
+++ src/utils/LP/Retarget/retargetMocapData.ts
@@ -0,0 +1,7 @@
+import { PlaskMocapData, PlaskRetargetMap } from 'types/common';
+
+const retargetMocapData = (assetId: string, retargetMap: PlaskRetargetMap, mocapData: PlaskMocapData) => {
+  console.log('mocapData: ', mocapData);
+};
+
+export default retargetMocapData;
