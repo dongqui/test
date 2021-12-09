81f27ef1cdcb9f79043cb19b1a050ea07f2adf82
CharmingCheol <toby0806@naver.com>
2021-12-09 12:24:55 +0900

    fix : 키프레임 복사 saga 시간차 계산하는 로직 수정

--- src/sagas/TP/keyframes/pasteKeyframes.ts
+++ src/sagas/TP/keyframes/pasteKeyframes.ts
@@ -1,6 +1,6 @@
 import { call, put, select, takeLatest } from 'redux-saga/effects';
 
-import { UpdatedPropertyKeyframes } from 'types/TP/keyframe';
+import { UpdatedPropertyKeyframes, ClusteredKeyframe } from 'types/TP/keyframe';
 import * as keyframesAction from 'actions/keyframes';
 import { RootState } from 'reducers';
 
@@ -14,11 +14,22 @@ function getCopiedPropertyKeyframes(state: RootState) {
   return state.keyframes.copiedPropertyKeyframes;
 }
 
+// 복사 된 property keyframes 중, frame이 가장 작은 값 찾기
+function findSmallestTime(copiedPropertyKeyframes: ClusteredKeyframe[]) {
+  let smallestFrame = Infinity;
+  copiedPropertyKeyframes.forEach((group) => {
+    const firstFrame = group.keyframes[0].time;
+    if (firstFrame < smallestFrame) smallestFrame = firstFrame;
+  });
+  return smallestFrame;
+}
+
 function* worker() {
-  const currentTimeIndex = getCurrentTimeIndex(yield select());
+  const scrubberTime = getCurrentTimeIndex(yield select());
   const copiedPropertyKeyframes = getCopiedPropertyKeyframes(yield select());
-  const updatedPropertyKeyframes: UpdatedPropertyKeyframes = yield call(setUpdatedPropertyKeyframes, copiedPropertyKeyframes, currentTimeIndex);
-  yield put(keyframesAction.paste({ currentTimeIndex }));
+  const smallestFrame = findSmallestTime(copiedPropertyKeyframes);
+  const updatedPropertyKeyframes: UpdatedPropertyKeyframes = yield call(setUpdatedPropertyKeyframes, copiedPropertyKeyframes, scrubberTime - smallestFrame);
+  yield put(keyframesAction.paste({ currentTimeIndex: scrubberTime }));
 
   // 이후부터 RP쪽 액션 호출
   // yield put(RP액션.키프레임복사(updatedPropertyKeyframes));
