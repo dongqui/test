65d7ab8447d539fd15faa3faafbbdb81e0a90ccc
kennycha <dudqn136@naver.com>
2021-12-28 14:55:43 +0900

    chore: delete sample containers

--- src/containers/Shoot.tsx
+++ src/containers/Shoot.tsx
@@ -13,9 +13,6 @@ import { useLSResizeState } from 'contexts/LS/ResizeContext';
 import Box, { BoxProps } from 'components/Layout/Box';
 import MiddleBar from './MiddleBar/Shoot';
 
-import DummyControlPanel from './Panels/DummyControlPanel';
-import DummyTimelinePanel from './Panels/DummyTimelinePanel';
-
 import HotKeyOrder from './HotKeyOrder';
 import classNames from 'classnames/bind';
 import styles from './Shoot.module.scss';
