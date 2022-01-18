3cd23d43a2d8a8f84c7be5b243c4ac675d548dde
dongqui <dongqui9@gmail.com>
2022-01-18 18:27:50 +0900

    chore: 불필요한 코드 삭제

--- src/containers/Panels/LibraryPanel/ListView/ListNode copy.tsx
+++ src/containers/Panels/LibraryPanel/ListView/ListNode copy.tsx
@@ -1,12 +1,10 @@
-import { find, filter, cloneDeep } from 'lodash';
-import React, { FunctionComponent, memo, Fragment, useEffect, useCallback, useState, useRef, DragEvent, FocusEvent } from 'react';
+import { find, filter } from 'lodash';
+import React, { FunctionComponent, memo, Fragment, useEffect, useCallback, useState, DragEvent } from 'react';
 import { useDispatch } from 'react-redux';
 import { useSelector } from 'reducers';
-import { ExportModal } from 'containers/Panels/LibraryPanel/Parts';
 import { forceClickAnimationPlayAndStop } from 'utils/common';
 import { goToSpecificPoses } from 'utils/RP';
 import * as animationDataActions from 'actions/animationDataAction';
-import { AnimationIngredient } from 'types/common';
 import ModelNode from '../Nodes/ModelNode';
 import FolderNode from '../Nodes/FolderNode';
 import MotionNode from '../Nodes/MotionNode';
@@ -17,17 +15,10 @@ interface Props {
 
 const ListNode: FunctionComponent<Props> = ({ node }) => {
   const dispatch = useDispatch();
-  const { type, name, filePath, id, assetId, parentId, childrens, extension } = node;
+  const { type, id, assetId, parentId, childrens } = node;
   const _assetList = useSelector((state) => state.plaskProject.assetList);
-  const _visualizedAssetIds = useSelector((state) => state.plaskProject.visualizedAssetIds);
   const _animationIngredients = useSelector((state) => state.animationData.animationIngredients);
   const _lpNode = useSelector((state) => state.lpNode.nodes);
-
-  const currentVisualizedNode = _lpNode.find((node) => node.assetId && _visualizedAssetIds.includes(node.assetId));
-  const currentVisualizedMotion = _animationIngredients.filter((ingredient) => ingredient.assetId === currentVisualizedNode?.assetId && ingredient.current);
-
-  const [currentMotions, setCurrentMotions] = useState<AnimationIngredient[]>([]);
-  const [isOpenExportModal, setIsOpenExportModal] = useState(false);
 
   const [isVisualizeCompleted, setIsVisualizeCompleted] = useState(false);
 
@@ -106,47 +97,11 @@ const ListNode: FunctionComponent<Props> = ({ node }) => {
     [_animationIngredients, _assetList, _lpNode, childrens.length, dispatch, id, parentId],
   );
 
-  const [showsChildrens, setShowsChildrens] = useState(false);
-
-  /**
-   * @TODO 아래의 코드는 clicked, visualized 스타일을 자식 노드를 포함하여 정의, 코드 개선이 필요
-   */
-
-  const currentVisualizedNodePath = (currentVisualizedNode?.filePath + `\\${currentVisualizedNode?.name}`).split('\\').filter((text) => !!text);
-  const currentNodePath = (filePath + `\\${name}`).split('\\').filter((text) => !!text);
-
-  let hasCurrentVisualizedNode = false;
-  currentNodePath.forEach((path, i) => {
-    if (path === currentVisualizedNodePath[i]) {
-      hasCurrentVisualizedNode = true;
-    } else {
-      hasCurrentVisualizedNode = false;
-    }
-  });
-
-  if (currentVisualizedNode) {
-  }
-
-  const isOpenVisualized = showsChildrens && hasCurrentVisualizedNode;
-
-  const isCloseVisualized =
-    type === 'Motion'
-      ? assetId && currentVisualizedMotion[0]?.assetId === assetId && currentVisualizedMotion[0]?.name === name
-      : type === 'Model'
-      ? !showsChildrens && assetId && _visualizedAssetIds.includes(assetId)
-      : !showsChildrens && hasCurrentVisualizedNode;
-  // style code END
-
-  const handleExportCancel = useCallback(() => {
-    setIsOpenExportModal(false);
-  }, []);
-
   return (
     <Fragment>
       {type === 'Model' && <ModelNode node={node} />}
       {type === 'Folder' && <FolderNode node={node} />}
       {type === 'Motion' && <MotionNode node={node} />}
-      {isOpenExportModal && <ExportModal motions={currentMotions} onCancel={handleExportCancel} onConfirm={() => {}} onOutsideClose={handleExportCancel} />}
     </Fragment>
   );
 };
