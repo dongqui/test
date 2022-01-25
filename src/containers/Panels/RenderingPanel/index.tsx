3117a3d09729272e7e754a4050f5b5f70299c8b1
kennycha <dudqn136@naver.com>
2022-01-25 21:14:45 +0900

    feat: animationGroup 생성 시 startTimeIndex로 이동 후 일시정지

--- src/containers/Panels/RenderingPanel/index.tsx
+++ src/containers/Panels/RenderingPanel/index.tsx
@@ -1334,10 +1334,11 @@ const RenderingPanel: FunctionComponent<Props> = () => {
       const newAnimationGroup = createAnimationGroupFromIngredient(visualizedAnimationIngredients[0], _fps, true);
 
       newAnimationGroup.normalize(_startTimeIndex, _endTimeIndex);
+      newAnimationGroup.start().goToFrame(_startTimeIndex).pause();
       dispatch(animatingControlsActions.setCurrentAnimationGroup({ animationGroup: newAnimationGroup }));
 
       return () => {
-        newAnimationGroup.pause();
+        newAnimationGroup.stop();
       };
     }
   }, [_animationIngredients, _endTimeIndex, _fps, _screenList, _startTimeIndex, _visualizedAssetIds, dispatch]);
