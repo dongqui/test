fe7ff2ad5c1cb3292d2d1d635454048d9466ed42
kennycha <dudqn136@naver.com>
2022-01-26 21:37:46 +0900

    feat: useEffect를 다시 animationGroup 생성용도로만 돌림

--- src/containers/Panels/RenderingPanel/index.tsx
+++ src/containers/Panels/RenderingPanel/index.tsx
@@ -1334,14 +1334,14 @@ const RenderingPanel: FunctionComponent<Props> = () => {
       const newAnimationGroup = createAnimationGroupFromIngredient(visualizedAnimationIngredients[0], _fps, true);
 
       newAnimationGroup.normalize(_startTimeIndex, _endTimeIndex);
-      newAnimationGroup.start().goToFrame(_startTimeIndex).pause();
+
       dispatch(animatingControlsActions.setCurrentAnimationGroup({ animationGroup: newAnimationGroup }));
 
       return () => {
         newAnimationGroup.stop();
       };
     }
-  }, [_animationIngredients, _endTimeIndex, _fps, _screenList, _startTimeIndex, _visualizedAssetIds, dispatch]);
+  }, [_animationIngredients, _endTimeIndex, _fps, _startTimeIndex, _visualizedAssetIds, dispatch]);
 
   /******************************************************************************
    * RP 내 하위 컨테이너들에 대한 내용
