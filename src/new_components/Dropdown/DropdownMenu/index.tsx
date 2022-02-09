928df91da4ed18f67361da6511648fa2f8695f2a
ChaMincheol <toby0806@naver.com>
2022-02-09 12:55:50 +0900

    refactor: 드랍다운 menu 컴포넌트 추가

---
+++ src/new_components/Dropdown/DropdownMenu/index.tsx
@@ -0,0 +1,76 @@
+import { useContext, useEffect, useRef, Fragment, FunctionComponent } from 'react';
+import { isEqual } from 'lodash';
+
+import { DropdownContext } from '../DropdownProvider';
+
+import classNames from 'classnames/bind';
+import styles from './index.module.scss';
+
+const cx = classNames.bind(styles);
+
+interface Props {}
+
+const DropdownMenu: FunctionComponent<Props> = (props) => {
+  const { children } = props;
+
+  const dropdownMenuRef = useRef<HTMLUListElement>(null);
+  const [_, dispatch] = useContext(DropdownContext);
+
+  // 드랍다운 메뉴 출력 시, window에 click/keydown/focus 이벤트 추가
+  useEffect(() => {
+    const currentRef = dropdownMenuRef.current;
+
+    if (currentRef) {
+      const handleOutSideClick = (event: MouseEvent) => {
+        const target = event.target as Node;
+        const isContains = currentRef.contains(target);
+        if (!isContains) {
+          dispatch('changeIsOpenMenu', { isOpenMenu: false });
+        }
+      };
+
+      const handleFocusin = (event: FocusEvent) => {
+        if (!currentRef.contains(event.target as Node)) {
+          event.preventDefault();
+        }
+      };
+
+      const handleKeyDown = (event: KeyboardEvent) => {
+        if (isEqual(event.key, 'Escape')) {
+          dispatch('changeIsOpenMenu', { isOpenMenu: false });
+        }
+
+        if (isEqual(event.key, 'Enter')) {
+          event.preventDefault();
+        }
+      };
+
+      window.addEventListener('click', handleOutSideClick);
+      window.addEventListener('focusin', handleFocusin);
+      window.addEventListener('keydown', handleKeyDown);
+
+      return () => {
+        window.removeEventListener('click', handleOutSideClick);
+        window.removeEventListener('focusin', handleFocusin);
+        window.removeEventListener('keydown', handleKeyDown);
+      };
+    }
+  }, [dispatch]);
+
+  return (
+    <ul className={cx('menu')} role="menu" ref={dropdownMenuRef}>
+      {children}
+    </ul>
+  );
+};
+
+const DropdownMenuWrapper: FunctionComponent<Props> = (props) => {
+  const { children } = props;
+
+  const [{ isOpenMenu }] = useContext(DropdownContext);
+
+  // isOpenMenu가 true인 경우에만 DropdownMenu와 children을 출력. false인 경우에는 출력 된 컴포넌트 소멸
+  return <Fragment>{isOpenMenu && <DropdownMenu>{children}</DropdownMenu>}</Fragment>;
+};
+
+export default DropdownMenuWrapper;
