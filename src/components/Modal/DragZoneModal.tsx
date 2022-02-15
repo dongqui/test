4e4c658f66c034951a7985c3e0b3e4c7f463b1b7
ChaMincheol <toby0806@naver.com>
2022-02-15 09:13:47 +0900

    feat: DragZonModal 컴포넌트 추가

--- src/components/Modal/DragZoneModal.tsx
+++ src/components/Modal/DragZoneModal.tsx
@@ -1,4 +1,5 @@
-import { FunctionComponent } from 'react';
+import React, { useRef, FunctionComponent } from 'react';
+import { useDropzone } from 'react-dropzone';
 
 import { FilledButton } from 'components/Button';
 import { IconWrapper, SvgPath } from 'components/Icon';
@@ -11,22 +12,66 @@ import styles from './DragZoneModal.module.scss';
 const cx = classnames.bind(styles);
 
 interface Props {
-  cancelText: string;
-
+  /**
+   * 모달의 제목
+   */
   title: string;
 
-  mainMessage: string;
+  /**
+   * 모달의 부제목
+   */
+  subTitle: string;
 
-  subMesaage: string;
+  /**
+   * 확장자 지원을 알려주는 메시지
+   */
+  extensionMesaage: string;
 
+  /**
+   * 취소 버튼 메시지
+   */
+  cancelButtonText: string;
+
+  /**
+   * 모달 취소 이벤트
+   */
   onCancel?: () => void;
 
+  /**
+   * 모달 종료 이벤트
+   */
   onClose: () => void;
+
+  /**
+   * 드래그 존에 드랍 이벤트
+   */
+  onDrop: (files: File[]) => void;
 }
 
 const DragZoneModal: FunctionComponent<Props> = (props) => {
-  const { cancelText, title, mainMessage, subMesaage, onCancel, onClose } = props;
+  const { cancelButtonText, title, subTitle, extensionMesaage, onCancel, onClose, onDrop } = props;
 
+  const { getRootProps } = useDropzone({ onDrop });
+
+  const fileExplorerRef = useRef<HTMLInputElement>(null);
+
+  // input file 변경
+  const handleInputFileChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    if (e.target.files !== null) {
+      const files = Array.from(e.target.files);
+      e.target.value = '';
+      onDrop(files);
+    }
+  };
+
+  // Select Files 텍스트 클릭
+  const handleSelectFIlesClick = () => {
+    if (fileExplorerRef.current) {
+      fileExplorerRef.current.click();
+    }
+  };
+
+  // 취소 버튼 클릭
   const handleCloseButtonClick = () => {
     onCancel && onCancel();
     onClose();
@@ -36,19 +81,20 @@ const DragZoneModal: FunctionComponent<Props> = (props) => {
     <BaseModal>
       <div className={cx('title')}>{title}</div>
       <div className={cx('content')}>
-        <Html content={mainMessage} />
-        <div className={cx('drag-zone-wrapper')}>
-          <span>{subMesaage}</span>
+        <Html content={subTitle} />
+        <div className={cx('drag-zone-wrapper')} {...getRootProps()}>
+          <span>{extensionMesaage}</span>
           <IconWrapper className={cx('cloud-upload-icon')} icon={SvgPath.CloudUpload} hasFrame={false} />
           <div className={cx('select-files-wrapper')}>
             <span>Drag & Drop here</span>
             <span>or</span>
-            <strong>Select files</strong>
+            <strong onClick={handleSelectFIlesClick}>Select files</strong>
+            <input type="file" multiple ref={fileExplorerRef} onChange={handleInputFileChange} />
           </div>
         </div>
       </div>
       <FilledButton className={cx('cancel-button')} onClick={handleCloseButtonClick}>
-        {cancelText}
+        {cancelButtonText}
       </FilledButton>
     </BaseModal>
   );
