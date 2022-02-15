65e3faec95c8f99a26b54247de9e261dd88d9668
ChaMincheol <toby0806@naver.com>
2022-02-15 09:14:31 +0900

    feat: LP file import 버튼 클릭 시 DragZone 모달 출력

--- src/containers/Panels/LibraryPanel/LPHeader.tsx
+++ src/containers/Panels/LibraryPanel/LPHeader.tsx
@@ -14,37 +14,24 @@ interface Props {
 const LPHeader: FunctionComponent<Props> = ({ onLoad }) => {
   const dispatch = useDispatch();
 
-  const handleChange = useCallback(
-    (e: ChangeEvent<HTMLInputElement>) => {
-      if (e.target.files !== null) {
-        const files = Array.from(e.target.files);
-        e.target.value = '';
-
-        onLoad(files);
-      }
-    },
-    [onLoad],
-  );
-
   // file import 버튼 클릭
   const handleFileImportButtonClick = useCallback(() => {
     dispatch(
       globalUIActions.openModal('DragZoneModal', {
         title: 'Import',
-        mainMessage: 'Import your 3D assets or source videos',
-        subMesaage: '.fbx, .glb, .mp4, .mov, .avi, .webm supported',
-        cancelText: 'Cancel',
+        subTitle: 'Import your 3D assets or source videos',
+        extensionMesaage: '.fbx, .glb, .mp4, .mov, .avi, .webm supported',
+        cancelButtonText: 'Cancel',
+        onDrop: onLoad,
       }),
     );
-  }, [dispatch]);
+  }, [dispatch, onLoad]);
 
   return (
     <div className={cx('wrapper')}>
       <div className={cx('title')}>library</div>
       <div className={cx('explorer')}>
         <IconWrapper className={cx('icon')} icon={SvgPath.Plus} hasFrame={false} onClick={handleFileImportButtonClick} />
-        {/* <label htmlFor="file-explorer" />
-        <input type="file" multiple id="file-explorer" onChange={handleChange} /> */}
       </div>
     </div>
   );
