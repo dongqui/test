97f3cfd227ed5367f010c95482a3157732b92b39
ChaMincheol <toby0806@naver.com>
2022-02-16 15:33:46 +0900

    style: scss에서 scss를 import해서 공통 스타일 관리

--- src/containers/Onboarding/OnboardingTooltip/case/ImportFileOnboarding.tsx
+++ src/containers/Onboarding/OnboardingTooltip/case/ImportFileOnboarding.tsx
@@ -1,7 +1,7 @@
 import { Fragment } from 'react';
 
 import classNames from 'classnames/bind';
-import styles from './index.module.scss';
+import styles from './ImportFileOnboarding.module.scss';
 
 const cx = classNames.bind(styles);
 
