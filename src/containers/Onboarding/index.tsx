99458721460730a3769bbccc2fbcf8a56924bbbb
ChaMincheol <toby0806@naver.com>
2022-03-02 16:22:02 +0900

    feature: 온보딩 2.0 컴포넌트 적용

--- src/containers/Onboarding/index.tsx
+++ src/containers/Onboarding/index.tsx
@@ -1,38 +1,69 @@
-import { useEffect, useState } from 'react';
+import { Fragment } from 'react';
 
 import { useSelector } from 'reducers';
 
-import LowerBanner from './LowerBanner';
+import { ApplyMotion, ExportFile, ImportFile, Keyframes, PropertySet, ResetOnboarding, RunOnboarding, VideoMode } from './Tooltip';
 
-import styles from './index.module.scss';
 import classNames from 'classnames/bind';
+import styles from './index.module.scss';
+
+/**
+ * 0: 온보딩 시작 단계
+ * 1~n: 온보딩 각 단계
+ * 999: 온보딩 마지막 단계(n+1값으로 하게 되면 온보딩 단계가 늘어날 때 마다 값을 변경해야 됨. 그래서 static하게 999으로 지정)
+ * null: 온보딩 미출력
+ * @default null null인 경우 온보딩 미출력
+ */
+export type OnboardingStep = 0 | 1 | 2 | 3 | 4 | 5 | 6 | 999 | null;
 
 const cx = classNames.bind(styles);
 
 const Onboarding = () => {
-  const isShowedOnboarding = useSelector((state) => state.globalUI.isShowedOnboarding);
-  const [isShowedLowerBanner, setIsShowedLowerBanner] = useState(false);
+  const onboardingStep = useSelector((state) => state.globalUI.onboardingStep);
 
-  // 1초 뒤에 하단 배너 출력
-  useEffect(() => {
-    if (isShowedOnboarding) {
-      setTimeout(() => {
-        setIsShowedLowerBanner(true);
-      }, 1000);
-    } else {
-      setIsShowedLowerBanner(false);
+  const TooltipCase = () => {
+    switch (onboardingStep) {
+      case 0: {
+        return <RunOnboarding />;
+      }
+      case 1: {
+        return <ImportFile />;
+      }
+      case 2: {
+        return <VideoMode />;
+      }
+      case 3: {
+        return <ApplyMotion />;
+      }
+      case 4: {
+        return <PropertySet />;
+      }
+      case 5: {
+        return <Keyframes />;
+      }
+      case 6: {
+        return <ExportFile />;
+      }
+      case 999: {
+        return <ResetOnboarding />;
+      }
+      default: {
+        return null;
+      }
     }
-  }, [isShowedOnboarding]);
+  };
 
-  if (!isShowedOnboarding) {
+  if (onboardingStep === null) {
     return null;
   }
 
   return (
-    <div className={cx('onboarding')}>
-      {isShowedLowerBanner && <LowerBanner />}
-      <div id="onboarding-tooltip-portal" />
-    </div>
+    <Fragment>
+      <div className={cx('tooltip')}>
+        <TooltipCase />
+      </div>
+      <div className={cx('background')} />
+    </Fragment>
   );
 };
 
