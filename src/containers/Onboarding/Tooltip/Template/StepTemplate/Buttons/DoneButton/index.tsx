3598eae57dfb58f122aed51c3b97b9c22a403f65
ChaMincheol <toby0806@naver.com>
2022-03-08 16:01:13 +0900

    feat: 온보딩 Template 컴포넌트 추가

---
+++ src/containers/Onboarding/Tooltip/Template/StepTemplate/Buttons/DoneButton/index.tsx
@@ -0,0 +1,32 @@
+import { useDispatch } from 'react-redux';
+
+import * as globalUIActions from 'actions/Common/globalUI';
+import { FilledButton } from 'components/Button';
+
+import classNames from 'classnames/bind';
+import styles from './index.module.scss';
+
+const cx = classNames.bind(styles);
+
+const DoneButton = () => {
+  const dispatch = useDispatch();
+
+  // 온보딩 쿠키 삽입
+  const setOnboardingLocalStorage = () => {
+    const localStorage = window.localStorage;
+    localStorage.setItem('onboarding_1', 'onboarding_1');
+  };
+
+  const handleDoneButtonClick = () => {
+    setOnboardingLocalStorage();
+    dispatch(globalUIActions.progressOnboarding({ onboardingStep: 999 }));
+  };
+
+  return (
+    <FilledButton onClick={handleDoneButtonClick} className={cx('done')}>
+      Done
+    </FilledButton>
+  );
+};
+
+export default DoneButton;
---
+++ src/containers/Onboarding/Tooltip/Template/StepTemplate/index.tsx
@@ -0,0 +1,67 @@
+import React, { forwardRef, Fragment, FunctionComponent } from 'react';
+
+import { OnboardingStep } from 'containers/Onboarding';
+
+import BaseTemplate from '../BaseTemplate';
+import { CancelButton, DoneButton, NextButton } from './Buttons';
+
+import classNames from 'classnames/bind';
+import styles from './index.module.scss';
+
+const cx = classNames.bind(styles);
+
+interface Props {
+  step: Exclude<OnboardingStep, 999 | null>;
+}
+
+const LAST_STEP = 6;
+
+const StartTemplate = () => {
+  return (
+    <div className={cx('start-template')}>
+      <div className={cx('buttons')}>
+        <Fragment>
+          <CancelButton text="No Thanks" />
+          <NextButton step={0} text="Show Me Around" />
+        </Fragment>
+      </div>
+    </div>
+  );
+};
+
+const ProgressTemplate: FunctionComponent<Props> = (props) => {
+  const { step } = props;
+
+  return (
+    <div className={cx('progress-template')}>
+      <span className={cx('step')}>
+        {step} of {LAST_STEP}
+      </span>
+      <div className={cx('buttons')}>
+        {step !== LAST_STEP ? (
+          <Fragment>
+            <CancelButton text="Cancel" />
+            <NextButton step={step} text="Next" />
+          </Fragment>
+        ) : (
+          <DoneButton />
+        )}
+      </div>
+    </div>
+  );
+};
+
+const StepTemplate = forwardRef<HTMLDivElement, Props & { children: React.ReactNode }>((props, ref) => {
+  const { children, step } = props;
+
+  return (
+    <BaseTemplate ref={ref}>
+      {children}
+      {step === 0 ? <StartTemplate /> : <ProgressTemplate step={step} />}
+    </BaseTemplate>
+  );
+});
+
+StepTemplate.displayName = 'StepTemplate';
+
+export default StepTemplate;
