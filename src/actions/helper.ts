d79b21f1f05156e1b11f0e11f08ec7a0e02223fb
plask <dongjin@plask.ai>
2022-03-21 18:53:58 +0900

    fix: handle -> update type명 변경

--- src/actions/helper.ts
+++ src/actions/helper.ts
@@ -3,19 +3,19 @@ import { TypeConstant, createAction } from 'typesafe-actions';
 // temporal type for example;
 interface SocketIOEvent {}
 
-export function createSocketActions<RequestActionArg, SendActionArg, ReceiveActionArg, FailureActionArg, HandleActionArg>(
+export function createSocketActions<RequestActionParam, SendActionParam, ReceiveActionParam, FailureActionParam, UpdateActionParam>(
   TRequestActionType: TypeConstant,
   TSendActionType: TypeConstant,
   TReceiveActionType: TypeConstant,
   TFailureActionType: TypeConstant,
-  THandleActionType: TypeConstant,
+  TUpdateActionType: TypeConstant,
 ) {
   return {
-    request: createAction(TRequestActionType)<RequestActionArg>(),
-    send: createAction(TSendActionType)<SendActionArg>(),
-    receive: createAction(TReceiveActionType)<ReceiveActionArg, SocketIOEvent>(),
-    failure: createAction(TFailureActionType)<FailureActionArg>(),
-    handle: createAction(THandleActionType)<HandleActionArg>(),
+    request: createAction(TRequestActionType)<RequestActionParam>(),
+    send: createAction(TSendActionType)<SendActionParam>(),
+    receive: createAction(TReceiveActionType)<ReceiveActionParam, SocketIOEvent>(),
+    failure: createAction(TFailureActionType)<FailureActionParam>(),
+    update: createAction(TUpdateActionType)<UpdateActionParam>(),
   };
 }
 
@@ -26,13 +26,13 @@ interface UpdateFolderRequestParam {}
 interface UpdateFolderSendParam {}
 interface UpdateFolderReceiveParam {}
 interface UpdateFolderFailureParam {}
-interface UpdateFolderHandleParam {}
+interface UpdateFolderUpdateParam {}
 
-const updateFolderSocketActions = createSocketActions<UpdateFolderRequestParam, UpdateFolderSendParam, UpdateFolderReceiveParam, UpdateFolderFailureParam, UpdateFolderHandleParam>(
+const updateFolderSocketActions = createSocketActions<UpdateFolderRequestParam, UpdateFolderSendParam, UpdateFolderReceiveParam, UpdateFolderFailureParam, UpdateFolderUpdateParam>(
   'node/UPDATE_FOLDER_SOCKET_REQUEST',
   'node/UPDATE_FOLDER_SOCKET_SEND',
   'node/UPDATE_FOLDER_SOCKET_RECEIVE',
-  'node/UPDATE_FOLDER_SOCKET_HANDLE',
+  'node/UPDATE_FOLDER_SOCKET_UPDATE',
   'node/UPDATE_FOLDER_SOCKET_FAILURE',
 );
 
@@ -52,12 +52,12 @@ function* updateFolderSend(action: ReturnType<typeof updateFolderSocketActions.s
 function* uppdateFolderReceive(action: ReturnType<typeof updateFolderSocketActions.receive>) {
   // const payloads = action.payload;
   // do something
-  // yield put(updateFolderSocketActions.handle(...something));
+  // yield put(updateFolderSocketActions.Update(...something));
 }
 
 // src/reducers/LP ---------------------------------------------------------------------------------------------------------
 // (reducer)
-// case: getType(updateFolderSocketActions.handle):
+// case: getType(updateFolderSocketActions.Update):
 //    update state!
 
 // somewhere in containers ---------------------------------------------------------------------------------------------------------
