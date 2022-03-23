89d3cd43d63e2dc249a5b6faf0f68f6b54ab4fcd
jjtjs159-jg <61176987+jjtjs159-jg@users.noreply.github.com>
2022-03-23 17:15:32 +0900

    feat(ssr): apply api to load scene data from server side

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,42 @@
+import { GetServerSideProps } from 'next';
 import dynamic from 'next/dynamic';
+import requestApi from 'api/requestApi';
 
 const DynamicWithNoSSR = dynamic(() => import('containers/index'), { ssr: false });
+
+export const getServerSideProps: GetServerSideProps = async (context) => {
+  const { query } = context;
+  const { token, sceneUid } = query;
+
+  const { loaded, data, error } = await requestApi({
+    method: 'GET',
+    url: `library/${sceneUid}`,
+    headers: {
+      Authorization: `bearer ${token}`,
+    },
+  })
+    .then((response) => {
+      return {
+        loaded: true,
+        error: null,
+        data: response.data,
+      };
+    })
+    .catch((error) => {
+      return {
+        loaded: false,
+        data: [],
+        error: error,
+      };
+    });
+
+  return {
+    props: {
+      loaded,
+      data,
+      error,
+    },
+  };
+};
 
 export default DynamicWithNoSSR;
