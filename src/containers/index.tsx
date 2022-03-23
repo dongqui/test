733876754291c49d83243885ea4370f844b0ec6f
ChaMincheol <toby0806@naver.com>
2022-03-23 10:17:19 +0900

    feature: 기존 액션 호출 및 useSelector 호출 코드 제거

--- src/containers/index.tsx
+++ src/containers/index.tsx
@@ -1,20 +1,10 @@
-import { FunctionComponent, memo, useEffect, useState } from 'react';
-import { useDispatch } from 'react-redux';
+import { FunctionComponent, memo, useState } from 'react';
 
-import * as commonActions from 'actions/Common/globalUI';
 import { ResizeProvider } from 'contexts/LS/ResizeContext';
-import { VideoMode } from 'containers/VideoMode';
-import { RootState, useSelector } from 'reducers';
 import { BabylonProvider } from 'contexts/RP/BabylonContext';
 import { PlaskEngine } from '3d/PlaskEngine';
 
-import Onboarding from './Onboarding';
-import Shoot from './Shoot';
-
-import classNames from 'classnames/bind';
-import styles from './index.module.scss';
-
-const cx = classNames.bind(styles);
+import Plask from './Plask';
 
 export type Procedure = 'service' | 'token' | 'success' | 'denied';
 
@@ -23,37 +13,15 @@ interface Props {
 }
 
 const Index: FunctionComponent<Props> = ({ browserType }) => {
-  const dispatch = useDispatch();
-
-  const { mode } = useSelector((state: RootState) => state.modeSelection);
-  const onboardingStep = useSelector((state) => state.globalUI.onboardingStep);
-
-  // 접속 후 2초 뒤에 온보딩 쿠키가 없을 경우, 온보딩 ui 출력
-  useEffect(() => {
-    setTimeout(() => {
-      const localStorage = window.localStorage;
-      if (!localStorage.getItem('onboarding_1')) {
-        dispatch(commonActions.progressOnboarding({ onboardingStep: 0 }));
-      }
-    }, 2000);
-  }, [dispatch]);
-
-  const [plaskEngine, setPlaskEngine] = useState(new PlaskEngine());
-
-  const classes = cx('wrapper', {
-    visible: mode === 'animationMode',
-    hidden: mode === 'videoMode',
-  });
+  const [plaskEngine] = useState(new PlaskEngine());
 
   return (
     <main>
       <ResizeProvider>
         <BabylonProvider plaskEngine={plaskEngine}>
-          <Shoot className={classes} />
+          <Plask browserType={browserType} />
         </BabylonProvider>
       </ResizeProvider>
-      {mode !== 'animationMode' && <VideoMode className={cx('wrapper')} browserType={browserType} />}
-      {onboardingStep !== null && <Onboarding />}
     </main>
   );
 };
