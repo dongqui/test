814a4b76d8900c5892408c929bfea2adbafe5144
jjtjs159-jg <61176987+jjtjs159-jg@users.noreply.github.com>
2022-03-24 16:41:45 +0900

    feat(containers): apply authentication error container

--- src/containers/index.tsx
+++ src/containers/index.tsx
@@ -4,25 +4,35 @@ import { ResizeProvider } from 'contexts/LS/ResizeContext';
 import { BabylonProvider } from 'contexts/RP/BabylonContext';
 import { PlaskEngine } from '3d/PlaskEngine';
 
+import { Authentication } from 'containers/Error';
+
 import Plask from './Plask';
 
 export type Procedure = 'service' | 'token' | 'success' | 'denied';
 
 interface Props {
   browserType: string;
+  error: {
+    statusCode: number;
+    message: string;
+    timestamp: string;
+    path: string;
+  };
 }
 
-const Index: FunctionComponent<Props> = ({ browserType }) => {
+const Index: FunctionComponent<Props> = ({ browserType, error }) => {
   const [plaskEngine] = useState(new PlaskEngine());
 
+  if (error) {
+    return <Authentication statusCode={error.statusCode} message={error.message} />;
+  }
+
   return (
-    <main>
-      <ResizeProvider>
-        <BabylonProvider plaskEngine={plaskEngine}>
-          <Plask browserType={browserType} />
-        </BabylonProvider>
-      </ResizeProvider>
-    </main>
+    <ResizeProvider>
+      <BabylonProvider plaskEngine={plaskEngine}>
+        <Plask browserType={browserType} />
+      </BabylonProvider>
+    </ResizeProvider>
   );
 };
 
