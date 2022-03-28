ef3e08f6b965b398ac64688984fa0ac7118e0f91
Benjamin Guignabert <benjamin.guignabert@gmail.com>
2022-03-28 11:41:02 +0200

    init

--- src/3d/modules/ik/IKModule.ts
+++ src/3d/modules/ik/IKModule.ts
@@ -1,3 +1,24 @@
+import { TransformNode } from '@babylonjs/core';
 import { Module } from '../Module';
+import { SelectorModule } from '../selector/SelectorModule';
 
-export class IKModule extends Module {}
+export class IKModule extends Module {
+  private _selectionChangeObserver: ReturnType<SelectorModule['onSelectionChangeObservable']['add']> = null;
+  private _activeTransformNodes: TransformNode[] = [];
+
+  private get _allTransformNodes() {
+    return this.plaskEngine.selectorModule.allTransformNodes;
+  }
+
+  public dispose() {
+    this.plaskEngine.selectorModule.onSelectionChangeObservable.remove(this._selectionChangeObserver);
+  }
+
+  public initialize() {
+    this._selectionChangeObserver = this.plaskEngine.selectorModule.onSelectionChangeObservable.add((objects) => this._onSelectionChange(objects));
+  }
+
+  private _onSelectionChange(objects: TransformNode[]) {
+    this._activeTransformNodes = objects;
+  }
+}
