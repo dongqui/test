30f1cbaf33e97caffd011dc4821d740e8cb9283b
plask <dongjin@plask.ai>
2022-04-04 13:58:05 +0900

    fix: fix getFileDepth bug

--- src/utils/LP/FileSystem/getFilePathDepth.ts
+++ src/utils/LP/FileSystem/getFilePathDepth.ts
@@ -1,10 +1,12 @@
+import _ from 'lodash';
+
 export default function getFilePathDepth(nodes: LP.Node[], targetNode: LP.Node) {
   let count = 1;
   let node = targetNode;
   while (node.parentId) {
     count += 1;
 
-    const parentNode = nodes.find((node) => node.id === targetNode.parentId);
+    const parentNode = _.find(nodes, { id: node.parentId });
     if (parentNode) {
       node = parentNode;
     } else {
