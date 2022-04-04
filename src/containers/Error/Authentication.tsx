320f636fc556f6b798700c64376405115e0127b6
jjtjs159-jg <jjtjs159@gmail.com>
2022-04-04 14:04:21 +0900

    feat(containers/error): apply linked button in authenication containers

--- src/containers/Error/Authentication.tsx
+++ src/containers/Error/Authentication.tsx
@@ -1,3 +1,5 @@
+import { LinkedButton } from 'components/Button';
+
 import styles from './Authentication.module.scss';
 import classNames from 'classnames/bind';
 
@@ -10,19 +12,24 @@ interface Props {
 
 const Authentication = ({ statusCode, message }: Props) => {
   console.log(statusCode, message);
-  // 401.1 -> invalid token
-  // 401.2 -> 토큰만료
-  // 400.99 -> 400 ->
+  // 401.1 -> Invalid token
+  // 401.2 -> Expired Token
+  // 400.99 -> 400 -> No Permission Scene
+  // ? -> Invalid scene id
   return (
     <div className={cx('wrapper')}>
       <div className={cx('headline')}>
         Oops! <br />
         The page does not exist
       </div>
-      <div className={cx('paragraph')}>The lionk you followed may be broken or the page may have been removed.</div>
+      <div className={cx('paragraph')}>The link you followed may be broken or the page may have been removed.</div>
       <div className={cx('button-group')}>
-        <button>Go Back</button>
-        <button>Go Home</button>
+        <LinkedButton href="javascript:window.history.back();" variant="filled" size="large">
+          Go Back
+        </LinkedButton>
+        <LinkedButton href="https://plask.ai" variant="outlined" size="large">
+          Home
+        </LinkedButton>
       </div>
     </div>
   );
