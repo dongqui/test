5ec2ed26713c5ab09600130f051ed8362b732d6b
plask <dongjin@plask.ai>
2022-05-02 18:40:51 +0900

    feat: init auth

--- src/sagas/socket.saga.ts
+++ src/sagas/socket.saga.ts
@@ -1,4 +1,3 @@
-import { editNodeNameSocket, applyMocapToModelSocket } from './../actions/LP/lpNodeAction';
 import { eventChannel, EventChannel } from 'redux-saga';
 import { call, fork, put, take, takeLatest, ChannelTakeEffect, all } from 'redux-saga/effects';
 import { io, Socket } from 'socket.io-client';
@@ -6,13 +5,7 @@ import { PayloadActionCreator } from 'typesafe-actions';
 
 import * as socketActions from 'actions/Common/socket';
 import * as lpActions from 'actions/LP/lpNodeAction';
-import { execPath } from 'process';
 
-const TEMP_SCENE_ID = 'ezl1xkrzgyd8n64dm4p9q2o03em5jv6j';
-const TEMP_TOKEN =
-  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiJ6OWtnengycDlubXJsNGprcDZ3Nzg1MXlkZTBxMzZ2ayIsImVtYWlsIjoiZ3lvQHBsYXNrLmFpIiwiand0VHlwZSI6ImxvZ2luIiwiaWF0IjoxNjUwNDMwNDk2LCJleHAiOjE3MjgxOTA0OTZ9.fpw55EMP1ABmK9BGun1r02reYS42JR-SUlBoHm2CNRw';
-
-type LibraryEventType = 'update' | 'delete' | 'move' | 'apply-mocap-to-model' | 'modify-retarget-map';
 type AnimationEventType =
   | 'rename'
   | 'modify-fps'
@@ -26,8 +19,8 @@ type AnimationEventType =
   | 'delete-frames';
 
 function createSocketIO(action: ReturnType<typeof socketActions.connectSocket.request>): Promise<Socket> {
-  const { scendId, token } = action.payload;
-  const socket = io(`wss://dev-socket-app.plask.ai/scenes?scenesId=${TEMP_SCENE_ID}&token=${TEMP_TOKEN}`, {
+  const { sceneId, token } = action.payload;
+  const socket = io(`wss://dev-socket-app.plask.ai/scenes?scenesId=${sceneId}&token=${token}`, {
     transports: ['websocket'],
   });
   return new Promise((resolve) => {
