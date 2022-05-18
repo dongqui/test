86b13e26a54f08510f3fda7582535ca873bc8439
jjtjs159-jg <jjtjs159@gmail.com>
2022-05-18 16:01:15 +0900

    feat: add video mode renewal container base layout

---
+++ src/containers/New_VideoMode/index.tsx
@@ -0,0 +1,70 @@
+import { useMemo } from 'react';
+import Box, { BoxProps } from 'components/Layout/Box';
+import { useWindowSize } from 'hooks/common';
+import classNames from 'classnames/bind';
+import styles from './index.module.scss';
+
+const cx = classNames.bind(styles);
+
+const VideoMode = () => {
+  const [windowWidth, windowHeight] = useWindowSize();
+
+  const boxProps = useMemo(
+    () => ({
+      US: {
+        height: windowHeight - 180,
+      } as BoxProps,
+      LS: {
+        height: 180,
+      } as BoxProps,
+      UP: {
+        height: 36,
+      } as BoxProps,
+      LP: {
+        width: 240,
+      } as BoxProps,
+      RP: {
+        height: windowHeight - 180,
+      } as BoxProps,
+      CP: {
+        width: 240,
+      } as BoxProps,
+      MB: {
+        height: 32,
+      } as BoxProps,
+      TP: {
+        height: 148,
+      } as BoxProps,
+    }),
+    [windowHeight],
+  );
+
+  return (
+    <div className={cx('wrapper')}>
+      <Box id="UP" {...boxProps.UP}>
+        UP
+      </Box>
+      <Box id="US" className={cx('upper-section')} {...boxProps.US}>
+        <Box id="LP" className={cx('library-panel')} {...boxProps.LP}>
+          LP
+        </Box>
+        <Box id="RP" className={cx('rendering-panel')} {...boxProps.RP}>
+          RP
+        </Box>
+        <Box id="CP" className={cx('control-panel')} {...boxProps.CP}>
+          CP
+        </Box>
+      </Box>
+      <Box id="LS" className={cx('lower-section')} {...boxProps.LS}>
+        <Box id="MB" {...boxProps.MB}>
+          MB
+        </Box>
+        <Box id="TP" {...boxProps.TP}>
+          TP
+        </Box>
+      </Box>
+    </div>
+  );
+};
+
+export default VideoMode;
