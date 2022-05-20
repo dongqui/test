80c233f43589d55647c02bb4e373f6994adf18b7
jjtjs159-jg <61176987+jjtjs159-jg@users.noreply.github.com>
2022-05-20 17:16:00 +0900

    feat: create base dropzone component

---
+++ src/components/Input/Dropzone/BaseDropzone.tsx
@@ -0,0 +1,53 @@
+import { Fragment, useCallback, ReactNode } from 'react';
+import { useDropzone } from 'react-dropzone';
+import classNames from 'classnames/bind';
+import styles from './BaseDropzone.module.scss';
+
+const cx = classNames.bind(styles);
+
+interface Props {
+  children?: ReactNode;
+  className?: string;
+  disabled?: boolean;
+  active?: string;
+  onDrop: (files: File[]) => void;
+}
+
+const BaseDropzone = ({ children, className, active, disabled, onDrop }: Props) => {
+  const handleDrop = useCallback(
+    (files: File[]) => {
+      onDrop(files);
+    },
+    [onDrop],
+  );
+
+  const { getRootProps, isDragActive } = useDropzone({ onDrop: handleDrop, disabled });
+
+  const dropzoneProps = useCallback(() => {
+    const baseStyles = cx('wrapper', className, {
+      /**
+       * Active styles are defined above, but if there are basic styles that can be defined in BaseDropzone, they will be added.
+       * @beta
+       */
+      active: isDragActive,
+    });
+
+    const activeStyles = `${baseStyles} ${active}`;
+
+    const classes = isDragActive ? activeStyles : baseStyles;
+
+    return {
+      ...getRootProps({
+        className: classes,
+      }),
+    };
+  }, [active, className, getRootProps, isDragActive]);
+
+  return (
+    <Fragment>
+      <div {...dropzoneProps()}>{children}</div>
+    </Fragment>
+  );
+};
+
+export default BaseDropzone;
