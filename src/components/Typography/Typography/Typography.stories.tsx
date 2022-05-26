cf628e7dd5a27251f10221fbca2cf1fb15ee9e18
Junhyeok99 <apple2870907@gmail.com>
2022-05-26 15:55:01 +0900

    storybook: add styles. add cases

--- src/components/Typography/Typography/Typography.stories.tsx
+++ src/components/Typography/Typography/Typography.stories.tsx
@@ -1,7 +1,11 @@
 import { Fragment } from 'react';
 import { ComponentMeta, ComponentStory } from '@storybook/react';
-
 import { Typography as Tg } from '../';
+
+import classNames from 'classnames/bind';
+import styles from './Typography.stories.module.scss';
+
+const cx = classNames.bind(styles);
 
 export default {
   component: Tg,
@@ -18,22 +22,33 @@ export default {
 
 const Template: ComponentStory<typeof Tg> = (args) => {
   return (
-    <Fragment>
-      <Tg component="h1" variant="title" className="typography">
-        Typography
-      </Tg>
-      <div style={{ zoom: 2 }}>
-        {typeof args.children === 'string' ? (
-          args.children.split('\n').map((v, i) => (
-            <Tg key={i} {...args}>
-              {v}
-            </Tg>
-          ))
-        ) : (
-          <Tg {...args} />
-        )}
+    <div className={cx('typo-story')}>
+      <div>
+        <Tg variant="title">simple usage</Tg>
+        <Tg>default</Tg>
+        <Tg variant="title">variant</Tg>
+        <Tg component="span" br>
+          break line
+        </Tg>
+        <Tg component="span">component</Tg>
       </div>
-    </Fragment>
+      <div>
+        <Tg component="h1" variant="title">
+          typography string children test
+        </Tg>
+        <div style={{ zoom: 1 }}>
+          {typeof args.children === 'string' ? (
+            args.children.split('\n').map((v, i) => (
+              <Tg key={i} {...args}>
+                {v}
+              </Tg>
+            ))
+          ) : (
+            <Tg {...args} />
+          )}
+        </div>
+      </div>
+    </div>
   );
 };
 export const Typography = Template.bind({});
@@ -41,8 +56,7 @@ export const Typography = Template.bind({});
 // TODO: add default props
 Typography.args = {
   br: false,
-  children: 'This is test typography',
+  children: 'This is test typography.',
   component: 'div',
   variant: 'title',
-  className: 'typography',
 };
