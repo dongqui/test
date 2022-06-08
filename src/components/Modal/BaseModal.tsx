c80b7fe6e56e5aac836f667c52b3fb79bab0dbef
Dongjin <dongjin@plask.ai>
2022-06-08 20:11:17 +0900

    chore: Base modal temporal props to handle padding

--- src/components/Modal/BaseModal.tsx
+++ src/components/Modal/BaseModal.tsx
@@ -1,6 +1,6 @@
 import { FunctionComponent, useRef, ReactNode, MutableRefObject } from 'react';
 import { BasePortal } from 'components/Modal';
-import { Overlay } from 'components/Overlay';
+
 import classnames from 'classnames/bind';
 import styles from './BaseModal.module.scss';
 
@@ -8,25 +8,21 @@ const cx = classnames.bind(styles);
 
 interface Props {
   children: ReactNode;
-  handleOutsideClose?: () => void;
   dataCy?: string;
+  // TODO: temporal props, should be reomoved
+  hasPadding?: boolean;
 }
 
-const BaseModal: FunctionComponent<Props> = ({ handleOutsideClose, dataCy, children }) => {
+const BaseModal: FunctionComponent<Props> = ({ dataCy, children, hasPadding = true }) => {
   const portalRef = useRef(document.getElementById('portal_modal')) as MutableRefObject<HTMLElement>;
   const modalRef = useRef<HTMLDivElement>(null);
-
-  const onClickOutside = () => {
-    handleOutsideClose && handleOutsideClose();
-  };
 
   return (
     <BasePortal container={portalRef}>
       <div data-cy={dataCy} className={cx('wrapper')} ref={modalRef}>
-        <div className={cx('inner')} tabIndex={0}>
+        <div className={cx('inner', { hasPadding })} tabIndex={0}>
           {children}
         </div>
-        <Overlay onClose={onClickOutside} />
       </div>
     </BasePortal>
   );
