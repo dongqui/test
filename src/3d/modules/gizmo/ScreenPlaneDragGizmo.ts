148b71228f442fb7936e1a31f9a42519881084fd
William Kim <william8012@naver.com>
2022-06-08 16:25:39 +0900

    minor fix on gizmo

--- src/3d/modules/gizmo/ScreenPlaneDragGizmo.ts
+++ src/3d/modules/gizmo/ScreenPlaneDragGizmo.ts
@@ -23,29 +23,15 @@ export class ScreenPlaneDragGizmo extends PlaneDragGizmo {
 
     // const scene = gizmoLayer.utilityLayerScene;
 
-    // let centerMesh: Mesh = CreateSphere('centerMesh', { diameter: 0.015 }, gizmoLayer.utilityLayerScene);
-    let centerMesh: Mesh = CreateBox('centerMesh', { size: 0.02 }, gizmoLayer.utilityLayerScene);
+    // let centerMesh: Mesh = CreateIcoSphere('centerMesh', { radius: 0.01, flat: false, subdivisions: 1 }, gizmoLayer.utilityLayerScene);
+    // let centerMesh: Mesh = CreateBox('centerMesh', { size: 0.015 }, gizmoLayer.utilityLayerScene);
+    let centerMesh: Mesh = CreateSphere('centerMesh', { diameter: 0.02 }, gizmoLayer.utilityLayerScene);
     // let centerMesh: Mesh = CreatePlane('centerMesh', gizmoLayer.utilityLayerScene);
     var unlit = new StandardMaterial('M_centerMesh', gizmoLayer.utilityLayerScene);
     unlit.disableLighting = true;
     unlit.emissiveColor = new Color3(1, 1, 1);
     centerMesh.material = unlit;
 
-    // TODO change mat while hover
-    // let actionManager = centerMesh.actionManager ?? new ActionManager(gizmoLayer.utilityLayerScene);
-    // actionManager.registerAction(
-    //   new ExecuteCodeAction(ActionManager.OnKeyDownTrigger, (event: ActionEvent) => {
-    //     let mat = centerMesh.material as StandardMaterial;
-    //     mat.emissiveColor = new Color3(1, 1, 0);
-    //   }),
-    // );
-
-    // actionManager.registerAction(
-    //   new ExecuteCodeAction(ActionManager.OnPointerOutTrigger, (event: ActionEvent) => {
-    //     let mat = centerMesh.material as StandardMaterial;
-    //     mat.emissiveColor = new Color3(1, 1, 1);
-    //   }),
-    // );
     this.setCustomMesh(centerMesh);
 
     // this.dragBehavior.onDragStartObservable.add(() => {
