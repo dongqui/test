df423661fc81a9c54db3b36b0e63ebacc3bfd205
Dongjin <dongqui9@gmail.com>
2022-07-05 19:06:43 +0900

    feat: popupManager init (wip)

--- src/utils/PopupManager.ts
+++ src/utils/PopupManager.ts
@@ -2,43 +2,72 @@ import { Dispatch } from 'redux';
 
 import * as commonActions from 'actions/Common/globalUI';
 
-// REF: https://www.figma.com/file/cjE07P97OvCTwOIornDmbK/Plask-Master-Design?node-id=2821%3A18043
+// Ref: https://www.figma.com/file/cjE07P97OvCTwOIornDmbK/Plask-Master-Design?node-id=2821%3A18043
 class PopupManager {
   isOnboardingDone: boolean;
   isNewFeatureModalDone: boolean;
+  isVMOnboardingDone: boolean;
+  proceedGenerator: Generator<void> | null;
+  dispatch: Dispatch | null;
 
   constructor() {
     this.isOnboardingDone = !!localStorage.getItem('onboarding_1');
     this.isNewFeatureModalDone = !!localStorage.getItem('notification');
+    this.isVMOnboardingDone = !!localStorage.getItem('onboarding_2');
+
+    this.proceedGenerator = null;
+    this.dispatch = null;
   }
 
-  *proceedAnimationpagePopup(dispatch: Dispatch) {
-    yield this.showNewFeatureModal(dispatch);
-    yield this.showOnboalding(dispatch);
-    yield this.showVmOnboarding(dispatch);
+  init(dispatch: Dispatch) {
+    this.proceedGenerator = this.proceedAnimationpagePopup();
+    this.dispatch = dispatch;
   }
 
-  showOnboalding(dispatch: Dispatch) {
-    dispatch(commonActions.progressOnboarding({ onboardingStep: 0 }));
+  next() {
+    this.proceedGenerator?.next();
   }
 
-  showNewFeatureModal(dispatch: Dispatch) {
-    dispatch(
-      commonActions.openModal('NotificationModal', {
-        message:
-          'You can save your work on Plask as of June 9th, 2022. See a list of savable items <a href="https://knowledge.plask.ai/en/scene_save" target="_blank" rel="noopener noreferrer">here.</a>',
-        title: 'New Feature! Auto Save',
-        closeCallback: () => {
-          if (!localStorage.getItem('onboarding_1')) {
-            dispatch(commonActions.progressOnboarding({ onboardingStep: 0 }));
-          }
-          localStorage.setItem('notification', 'true');
-        },
-      }),
-    );
+  *proceedAnimationpagePopup() {
+    if (!this.isNewFeatureModalDone) {
+      yield this.showNewFeatureModal();
+    }
+    if (!this.isOnboardingDone) {
+      yield this.showOnboalding();
+    }
+    if (!this.isVMOnboardingDone) {
+      yield this.showVmOnboarding();
+    }
   }
 
-  showVmOnboarding(dispatch: Dispatch) {}
+  showOnboalding() {
+    setTimeout(() => {
+      if (this.dispatch) {
+        this.dispatch(commonActions.progressOnboarding({ onboardingStep: 0 }));
+      }
+    }, 2000);
+  }
+
+  showNewFeatureModal() {
+    if (this.dispatch) {
+      this.dispatch(
+        commonActions.openModal('NotificationModal', {
+          message:
+            'You can save your work on Plask as of June 9th, 2022. See a list of savable items <a href="https://knowledge.plask.ai/en/scene_save" target="_blank" rel="noopener noreferrer">here.</a>',
+          title: 'New Feature! Auto Save',
+          closeCallback: () => {
+            this.next();
+          },
+        }),
+      );
+    }
+  }
+
+  showVmOnboarding() {
+    if (this.dispatch) {
+      const isExpierencedUser = this.isOnboardingDone;
+    }
+  }
 }
 
 export default new PopupManager();
