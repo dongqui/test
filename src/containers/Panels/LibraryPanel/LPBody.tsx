051294c237125b9b8a25892727ea5739d2687a55
jjtjs159-jg <jjtjs159@gmail.com>
2022-07-18 19:19:08 +0900

    feat: add LP scrollbar

--- src/containers/Panels/LibraryPanel/LPBody.tsx
+++ src/containers/Panels/LibraryPanel/LPBody.tsx
@@ -43,88 +43,17 @@ const LPBody: FunctionComponent<Props> = ({ lpNodes }) => {
     dispatch(lpNodeActions.moveNodeSocket.request(''));
   };
 
-  const innerRef = useRef<HTMLDivElement>(null);
-  const [scrollHandleY, setScrollHandleY] = useState(0);
-  const prevScrollY = useRef(0);
-
-  const [goingUp, setGoingUp] = useState(false);
-
-  const [scrollHandleHeight, setScrollHandleHeight] = useState(0);
-  const [isScrollHidden, setIsScrollHidden] = useState(true);
-
-  useEffect(() => {
-    const currentRef = innerRef.current;
-    const libraryPanel = document.getElementById('LP-Body');
-
-    const handleScroll = (e: Event) => {
-      const target = e.target as HTMLDivElement;
-
-      if (target) {
-        const currentScrollY = target.scrollTop;
-        console.log(target.scrollTop);
-
-        if (prevScrollY.current < currentScrollY) {
-          setScrollHandleY(prevScrollY.current);
-        }
-
-        if (prevScrollY.current > currentScrollY) {
-          setScrollHandleY(currentScrollY);
-        }
-
-        prevScrollY.current = currentScrollY;
-      }
-    };
-
-    const contentResizeObserver = new ResizeObserver(() => {
-      if (currentRef && libraryPanel) {
-        const panelHeight = libraryPanel.offsetHeight;
-        const contentHeight = currentRef.scrollHeight;
-
-        const ratio = panelHeight / contentHeight;
-        const handleHeight = panelHeight * ratio;
-
-        setIsScrollHidden(ratio >= 1);
-        setScrollHandleHeight(handleHeight);
-      }
-    });
-
-    if (currentRef && libraryPanel) {
-      contentResizeObserver.observe(currentRef);
-
-      for (var i = 0; i < currentRef.children.length; i++) {
-        contentResizeObserver.observe(currentRef.children[i]);
-      }
-
-      currentRef.addEventListener('scroll', handleScroll, { passive: true });
-
-      return () => {
-        contentResizeObserver.unobserve(currentRef);
-        currentRef.removeEventListener('scroll', handleScroll);
-      };
-    }
-  }, [scrollHandleY]);
-
   const rootPathNodes = (lpNodes ?? nodes).filter((node) => !node.parentId).sort((a, b) => new Date(a.createdAt).getTime() - new Date(b.createdAt).getTime());
 
   return (
     <div className={cx('inner')} onContextMenu={handleContextMenu} onClick={handleClick} onDrop={handleDrop} data-cy="lp-body">
-      <div className={cx('content')} ref={innerRef}>
-        <div className={cx('onboarding-export-file-target')} id={ONBOARDING_ID.EXPORT_FILE} />
-        <div className={cx('onboarding-apply-motion-target')} id={ONBOARDING_ID.APPLY_MOTION} />
-        {rootPathNodes.map((node) => (
-          <div className={cx('node-row')} key={node.id}>
-            <ListNode node={node} />
-          </div>
-        ))}
-        <div className={cx('scrollbar-wrapper')}>
-          <div className={cx('scrollbar-track')}>
-            <div
-              className={cx('scrollbar-handle')}
-              style={{ display: isScrollHidden ? 'none' : 'block', height: `${scrollHandleHeight}px`, transform: `translateY(${scrollHandleY}px)` }}
-            />
-          </div>
+      <div className={cx('onboarding-export-file-target')} id={ONBOARDING_ID.EXPORT_FILE} />
+      <div className={cx('onboarding-apply-motion-target')} id={ONBOARDING_ID.APPLY_MOTION} />
+      {rootPathNodes.map((node) => (
+        <div className={cx('node-row')} key={node.id}>
+          <ListNode node={node} />
         </div>
-      </div>
+      ))}
     </div>
   );
 };
