acfd2a58e7b431a3c44c7ad9db9b301cc0981f81
Dongjin <dongqui9@gmail.com>
2022-08-10 23:12:20 +0900

    feat: add user state - hadFreeTrial

--- src/actions/User/index.ts
+++ src/actions/User/index.ts
@@ -1,8 +1,12 @@
-import { createAsyncAction } from 'typesafe-actions';
+import { createAsyncAction, createAction } from 'typesafe-actions';
 
 import { UserUsageInfoReponse } from 'types/common';
 
-export const getUserAsync = createAsyncAction('user/GET_USER_REQUEST', 'user/GET_USER_SUCCESS', 'user/GET_USER_FAILURE')<undefined, string, Error>();
+export const getUserAsync = createAsyncAction('user/GET_USER_REQUEST', 'user/GET_USER_SUCCESS', 'user/GET_USER_FAILURE')<
+  undefined,
+  { name: string; hadFreeTrial: boolean },
+  Error
+>();
 export const getUserUsagaInfoAsync = createAsyncAction('user/GET_USER_USAGE_INFO_REQUEST', 'user/GET_USER_USAGE_INFO_SUCCESS', 'user/GET_USER_USAGE_INFO_FAILURE')<
   undefined,
   Partial<UserUsageInfoReponse>,
