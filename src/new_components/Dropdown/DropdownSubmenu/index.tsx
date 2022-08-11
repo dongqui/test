53412a86e531912b8d3f33bfac3e6939034db445
Dongjin <dongqui9@gmail.com>
2022-08-11 18:26:54 +0900

    feat: dropdown submenu

---
+++ src/new_components/Dropdown/DropdownSubmenu/index.tsx
@@ -0,0 +1,89 @@
+import React, { useContext, useEffect, useRef, useState, ReactNode } from 'react';
+
+import { DropdownContext } from '../DropdownProvider';
+
+import classNames from 'classnames/bind';
+import styles from './index.module.scss';
+
+const cx = classNames.bind(styles);
+
+interface Position {
+  left?: string | number;
+  right?: string | number;
+  top?: string | number;
+  bottom?: string | number;
+}
+
+interface Props {
+  label: ReactNode;
+  arrow?: ReactNode;
+  disabled?: boolean;
+  children: ReactNode;
+  classNames?: string;
+}
+
+const SUBMENU_OFFSET = '100%';
+
+const DropdownSubmenu = ({ children, label, arrow = '>', classNames }: Props) => {
+  const nodeRef = useRef<HTMLDivElement>(null);
+  const [showSubmenu, setShowSubmenu] = useState(false);
+  const [position, setPosition] = useState<Position>({
+    top: 0,
+    right: undefined,
+    bottom: 'initial',
+    left: SUBMENU_OFFSET,
+  });
+  const [_, dispatch] = useContext(DropdownContext);
+
+  useEffect(() => {
+    if (nodeRef.current) {
+      const { innerWidth, innerHeight } = window;
+      const rect = nodeRef.current.getBoundingClientRect();
+      const style: Position = {};
+
+      if (rect.right < innerWidth) {
+        style.left = SUBMENU_OFFSET;
+        style.right = undefined;
+      } else {
+        style.right = SUBMENU_OFFSET;
+        style.left = undefined;
+      }
+
+      if (rect.bottom > innerHeight) {
+        style.bottom = 0;
+        style.top = 'initial';
+      } else {
+        style.bottom = 'initial';
+      }
+
+      setPosition(style);
+    }
+  }, []);
+
+  const handleMouseEnter = () => {
+    setShowSubmenu(true);
+  };
+
+  const handleMouseLeave = () => {
+    setShowSubmenu(false);
+  };
+
+  const submenuStyle = {
+    ...position,
+  };
+
+  return (
+    <div className={cx('wrapper', classNames)} onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}>
+      <div className={cx('trigger')}>
+        {label}
+        <span className={cx('arrow')}>{arrow}</span>
+      </div>
+      {showSubmenu && (
+        <div className={cx('submenu')} ref={nodeRef} style={submenuStyle}>
+          {children}
+        </div>
+      )}
+    </div>
+  );
+};
+export default DropdownSubmenu;
