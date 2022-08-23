7f8d4b4ae4bedc872a6c2d7aefd81d5235251863
Dongjin <dongqui9@gmail.com>
2022-08-23 10:17:01 +0900

    feat: setTimeout hook

--- src/hooks/common/index.ts
+++ src/hooks/common/index.ts
@@ -2,3 +2,4 @@ export { default as useHover } from './useHover';
 export { default as useWindowSize } from './useWindowSize';
 export { default as useMediaStream } from './useMediaStream';
 export { default as useInterval } from './useInterval';
+export { default as useSetTimeout } from './useSetTimeout';
