ea4d46eaafca7402ee7e74b1402d63f73d8a7520
Dongjin <dongqui9@gmail.com>
2022-08-23 10:41:58 +0900

    feat: add link, correct word (main logo dropdown)

--- src/containers/UpperBar/DropDown/MainLogoDropDown/index.tsx
+++ src/containers/UpperBar/DropDown/MainLogoDropDown/index.tsx
@@ -8,22 +8,20 @@ import styles from './index.module.scss';
 const cx = classNames.bind(styles);
 
 const MainLogoDropDown = () => {
-  const handleSelectGoToDashboard = () => {};
-  const handleSelectGoToHompage = () => {};
   return (
     <Dropdown>
       <Dropdown.Header>
         <ExpandButton content={SvgPath.Logo} type="ghost" />
       </Dropdown.Header>
       <Dropdown.Menu>
-        <Dropdown.Item menuItem="Onboarding" onClick={handleSelectGoToDashboard}>
-          Go to dashboard
+        <Dropdown.Item menuItem="Onboarding">
+          <a href={process.env.NEXT_PUBLIC_DASHBOARD_URL}>Go to dashboard</a>
         </Dropdown.Item>
-        <Dropdown.Submenu label="Help & Feddback" classNames={cx('main-logo-sub-menu')}>
+        <Dropdown.Submenu label="Help & Feedback" classNames={cx('main-logo-sub-menu')}>
           <HelpMenus />
         </Dropdown.Submenu>
-        <Dropdown.Item menuItem="Onboarding" onClick={handleSelectGoToDashboard}>
-          Go to homepage
+        <Dropdown.Item menuItem="Onboarding">
+          <a href={process.env.NEXT_PUBLIC_HOMEPAGE_URL}>Go to homepage</a>
         </Dropdown.Item>
       </Dropdown.Menu>
     </Dropdown>
