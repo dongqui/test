2366245b110f58922c776b8df92afbbbeba50192
Dongjin <dongqui9@gmail.com>
2022-08-26 21:28:35 +0900

    feat: add new features noti

--- src/components/Modal/NotificationModal.tsx
+++ src/components/Modal/NotificationModal.tsx
@@ -32,7 +32,7 @@ export default function NotificationModal({ onClose, title, message, closeCallba
         </header>
         <div className={cx('body')}>
           <video autoPlay loop muted width="640" height="360">
-            <source src="/video/vm_update.mp4" type="video/mp4" />
+            <source src="/video/dashboard_update.mp4" type="video/mp4" />
           </video>
         </div>
         <footer className={cx('footer')}>
