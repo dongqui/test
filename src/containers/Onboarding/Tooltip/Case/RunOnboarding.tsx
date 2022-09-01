2c3259b1d8d92e39798b3be2ebb9d31fcac0814e
Junhyeok99 <apple2870907@gmail.com>
2022-09-01 13:43:33 +0900

    fix: replace onboarding 0 case

--- src/containers/Onboarding/Tooltip/Case/RunOnboarding.tsx
+++ src/containers/Onboarding/Tooltip/Case/RunOnboarding.tsx
@@ -1,12 +1,18 @@
 import { useEffect, useRef } from 'react';
 import StepTemplate from '../Template/StepTemplate';
+import { ONBOARDING_ID } from 'containers/Onboarding/id';
+import { getTargetCoordinates } from 'utils/common';
 
 const RunOnboarding = () => {
   const tooltipRef = useRef<HTMLDivElement>(null);
 
   useEffect(() => {
     if (tooltipRef.current) {
-      tooltipRef.current.style.cssText = `top:46px; left:44px;`;
+      const helpButton = document.getElementById(ONBOARDING_ID.HELP_BUTTON);
+      const targetCoordinates = getTargetCoordinates(helpButton);
+      if (targetCoordinates) {
+        tooltipRef.current.style.cssText = `top: ${targetCoordinates.rightBottom.y + 10}px; right: calc(100vw - ${targetCoordinates.rightBottom.x + 6}px);`;
+      }
     }
   }, [tooltipRef]);
 
