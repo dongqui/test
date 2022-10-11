99dcdc7e596a64a9cc9a9f2af329518e1613a531
Junhyeok99 <apple2870907@gmail.com>
2022-10-11 09:14:58 +0900

    fix: GTM unlock_achievement event

--- src/containers/Onboarding/Tooltip/Template/StepTemplate/index.tsx
+++ src/containers/Onboarding/Tooltip/Template/StepTemplate/index.tsx
@@ -17,10 +17,32 @@ const StepTemplate = forwardRef<HTMLDivElement, StepTemplateProps>((props, ref) 
   const { children, step } = props;
   useEffect(() => {
     if (step >= 1 && step <= 6) {
+      let id = '';
+      switch (step) {
+        case 1:
+          id = 'import';
+          break;
+        case 2:
+          id = 'shift_vm';
+          break;
+        case 3:
+          id = 'apply_mocap';
+          break;
+        case 4:
+          id = 'edit_cp';
+          break;
+        case 5:
+          id = 'edit_keyframe';
+          break;
+        case 6:
+          id = 'export';
+          break;
+      }
+
       TagManager.dataLayer({
         dataLayer: {
           event: 'unlock_achievement',
-          achievement_id: `am_onboarding_${step.toString().padStart(2, '0')}`,
+          achievement_id: id,
         },
       });
     }
