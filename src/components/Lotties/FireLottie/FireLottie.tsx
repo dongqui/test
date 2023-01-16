44e11132eb8697a69794465dce28fdd5219e59b1
Dongjin <dongjin@plask.ai>
2023-01-16 17:56:28 +0900

    feat: add lottie, icon

---
+++ src/components/Lotties/FireLottie/FireLottie.tsx
@@ -0,0 +1,6 @@
+import Lottie from 'react-lottie-player';
+import data from './json';
+
+export default function FireLottie() {
+  return <Lottie animationData={data} speed={1} style={{ height: 106 }} loop play />;
+}
