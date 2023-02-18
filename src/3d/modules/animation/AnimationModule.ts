807e02896e63a8c4dad693ff180c2dc331dbf93f
정준혁(Junhyeok Jung) <33452763+Junhyeok99@users.noreply.github.com>
2023-02-18 23:22:26 +0900

    Revert "Revert "Revert "fixed compute formula"""

--- src/3d/modules/animation/AnimationModule.ts
+++ src/3d/modules/animation/AnimationModule.ts
@@ -550,7 +550,7 @@ export class AnimationModule extends Module {
 
     const { tracks } = baseLayer;
     const { hipSpace } = retargetMap;
-    console.log('hipSpace: ', hipSpace);
+
     const contactData = [];
 
     // iterate mocapData not tracks for efficiency
@@ -602,8 +602,7 @@ export class AnimationModule extends Module {
             }
             transformKeys.forEach((transformKey) => {
               const { frame, value } = transformKey;
-              const newValue = (value as ArrayOfThreeNumbers).map((v, idx) => (idx === 2 ? ((v * 100 - 106) * hipSpace) / 106 : (v * 100 * hipSpace) / 106));
-
+              const newValue = (value as ArrayOfThreeNumbers).map((v, idx) => (v * hipSpace) / 106);
               targetTrack.transformKeys.push({ frame, value: Vector3.TransformCoordinates(Vector3.FromArray(newValue), transformMatrix) }); // the root mesh is scaled down to 1/100, all transformKeys have to have 100 * value
             });
           }
